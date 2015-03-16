cdbe55526a71956614294c8e5c52da0d8661ae4e
Martyn Taylor <mtaylor@redhat.com>
2015-03-16 12:34:25 +0000

    License updates including Base64 and JSON.
    Updates headers and license files for Base64.java and JSON library.

--- activemq-commons/src/main/java/org/apache/activemq/utils/Base64.java
+++ activemq-commons/src/main/java/org/apache/activemq/utils/Base64.java
@@ -1,18 +1,13 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * This file is released into the public domain.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Notice from author (Robert Harder, rob@iharder.net):
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * "I have released this software into the Public Domain. That means you can do whatever you want with it. Really. You
+ * don't have to match it up with any other open source license &em; just use it. You can rename the files, move the
+ * Java packages, whatever you want. If your lawyers say you have to have a license, contact me, and I'll make a special
+ * release to you under whatever reasonable license you desire: MIT, BSD, GPL, whatever."
+ *
  */
 package org.apache.activemq.utils;
 
