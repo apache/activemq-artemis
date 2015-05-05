56e4bd79bc01129c907852d53b2c299e60b04738
Andy Taylor <andy.tayls67@gmail.com>
2015-05-05 11:08:52 +0100

    use host argument properly with CLI create command

--- artemis-cli/src/main/java/org/apache/activemq/artemis/cli/commands/Create.java
+++ artemis-cli/src/main/java/org/apache/activemq/artemis/cli/commands/Create.java
@@ -94,7 +94,7 @@ public class Create implements Action
       context.out.println(String.format("Creating ActiveMQ Artemis instance at: %s", directory.getCanonicalPath()));
       if (host == null)
       {
-         host = directory.getName();
+         host = "localhost";
       }
 
       HashMap<String, String> filters = new HashMap<String, String>();
