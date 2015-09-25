8bb3d1ceaa5bd1b3138286c1777805535428d9f2
Clebert Suconic <clebertsuconic@apache.org>
2015-09-25 18:09:08 -0400

    Improving into Stomp Embedded Interceptor example Using the regular maven plugin framework, and adding the interceptors to the regular broker.xml configuration

--- examples/protocols/stomp/stomp-embedded-interceptor/src/main/java/org/apache/activemq/artemis/jms/example/StompEmbeddedWithInterceptorExample.java
+++ examples/protocols/stomp/stomp-embedded-interceptor/src/main/java/org/apache/activemq/artemis/jms/example/StompEmbeddedWithInterceptorExample.java
@@ -16,67 +16,24 @@
  */
 package org.apache.activemq.artemis.jms.example;
 
-import org.apache.activemq.artemis.api.core.ActiveMQException;
-import org.apache.activemq.artemis.api.core.TransportConfiguration;
-import org.apache.activemq.artemis.core.config.Configuration;
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.config.impl.SecurityConfiguration;
-import org.apache.activemq.artemis.core.protocol.stomp.StompConnection;
-import org.apache.activemq.artemis.core.protocol.stomp.StompFrame;
-import org.apache.activemq.artemis.core.protocol.stomp.StompFrameInterceptor;
-import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
-import org.apache.activemq.artemis.jms.server.embedded.EmbeddedJMS;
-import org.apache.activemq.artemis.jms.server.config.ConnectionFactoryConfiguration;
-import org.apache.activemq.artemis.jms.server.config.impl.ConnectionFactoryConfigurationImpl;
-import org.apache.activemq.artemis.spi.core.protocol.RemotingConnection;
-import org.apache.activemq.artemis.spi.core.security.ActiveMQSecurityManagerImpl;
-
 import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
+import javax.jms.Message;
 import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
 import javax.jms.Session;
-import javax.jms.TextMessage;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.HashSet;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 /**
  * This example demonstrates how to run an ActiveMQ Artemis embedded with JMS
  */
 public class StompEmbeddedWithInterceptorExample {
 
-   public static class MyStompInterceptor
-      implements StompFrameInterceptor {
-
-      public boolean intercept(StompFrame frame, RemotingConnection remotingConnection)
-         throws ActiveMQException {
-
-         StompConnection connection = (StompConnection) remotingConnection;
-
-         System.out.println("Intercepted frame " + frame + " on connection " + connection);
-
-         return false;
-      }
-   }
-
    private static final String END_OF_FRAME = "\u0000";
 
    public static void main(final String[] args) throws Exception {
-      EmbeddedJMS jmsServer = new EmbeddedJMS(); 
-      String brokerConfigPath = StompEmbeddedWithInterceptorExample.class.getResource("/broker.xml").toString();
-      jmsServer.setConfigResourcePath(brokerConfigPath);
-
-      jmsServer.start();
-      jmsServer.getActiveMQServer().getRemotingService().addIncomingInterceptor(new MyStompInterceptor());
-
-      System.out.println("Started Embedded JMS Server");
-
       // Step 1. Create a TCP socket to connect to the Stomp port
       Socket socket = new Socket("localhost", 61616);
 
@@ -95,7 +52,7 @@ public class StompEmbeddedWithInterceptorExample {
       // jms.queue.exampleQueue address with a text body
       String text = "Hello World from Stomp 1.2 !";
       String message = "SEND\n" +
-         "destination:jms.queue.exampleQueue\n" +
+         "destination:jms.queue.exampleQueue" +
          "\n" +
          text +
          END_OF_FRAME;
@@ -111,8 +68,28 @@ public class StompEmbeddedWithInterceptorExample {
       // Step 5. Slose the TCP socket
       socket.close();
 
-      Thread.sleep(1000);
-      jmsServer.stop();
+
+      // It will use a regular JMS connection to show how the injected data will appear at the final message
+
+      ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+      Connection connection = factory.createConnection();
+      Session session = connection.createSession();
+      connection.start();
+
+      MessageConsumer consumer = session.createConsumer(session.createQueue("exampleQueue"));
+      Message messageReceived = consumer.receive(5000);
+
+      String propStomp = messageReceived.getStringProperty("stompIntercepted");
+
+      String propRegular = messageReceived.getStringProperty("regularIntercepted");
+
+      System.out.println("propStomp is Hello!! - " + propStomp.equals("Hello"));
+      System.out.println("propRegular is HelloAgain!! - " + propRegular.equals("HelloAgain"));
+
+      session.close();
+      connection.close();
+
+      factory.close();
    }
 
    private static void sendFrame(Socket socket, String data) throws Exception {
