2c0f1df827b8f255a5f76f9105cf95b2e6b28e72
Erich Duda <eduda@redhat.com>
2016-01-12 16:52:06 +0100

    ARTEMIS-337 - [Artemis Testsuite] LiveToLiveFailoverTest.testNonTransactedWithZeroConsumerWindowSize fails

--- tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/cluster/failover/FailoverTest.java
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/cluster/failover/FailoverTest.java
@@ -466,7 +466,7 @@ public class FailoverTest extends FailoverTestBase {
 
       crash(session);
 
-      Assert.assertTrue(latch.await(1, TimeUnit.SECONDS));
+      Assert.assertTrue(latch.await(10, TimeUnit.SECONDS));
 
    }
 
