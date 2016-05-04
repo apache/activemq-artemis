b5c769a645163e5c7c998276b47f01fee0b2a44a
Martyn Taylor <mtaylor@redhat.com>
2016-05-04 19:28:06 +0100

    Switch File Based Large Message Tests

--- tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/largemessage/LargeMessageTestBase.java
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/largemessage/LargeMessageTestBase.java
@@ -81,9 +81,7 @@ public abstract class LargeMessageTestBase extends ActiveMQTestBase {
 
    @Parameterized.Parameters(name = "storeType={0}")
    public static Collection<Object[]> data() {
-//      Object[][] params = new Object[][] {{StoreConfiguration.StoreType.FILE}, {StoreConfiguration.StoreType.DATABASE}};
-      Object[][] params = new Object[][] {{StoreConfiguration.StoreType.DATABASE}};
-      //Object[][] params = new Object[][] {{StoreConfiguration.StoreType.FILE}};
+      Object[][] params = new Object[][]{{StoreConfiguration.StoreType.FILE}, {StoreConfiguration.StoreType.DATABASE}};
       return Arrays.asList(params);
    }
 
