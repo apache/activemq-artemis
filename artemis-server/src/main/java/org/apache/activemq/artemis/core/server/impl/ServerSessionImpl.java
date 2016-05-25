af4aa9fcb67431dc04ac6d13584391925f691ae4
Clebert Suconic <clebertsuconic@apache.org>
2016-05-25 13:46:26 -0400

    ARTEMIS-539 ServerSession defaultAddress can get out of sync after full queues

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ServerSessionImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ServerSessionImpl.java
@@ -1233,9 +1233,7 @@ public class ServerSessionImpl implements ServerSession, FailureListener {
 
       SimpleString address = message.getAddress();
 
-      if (defaultAddress == null && address != null) {
-         defaultAddress = address;
-      }
+      checkDefaultAddress(address);
 
       if (address == null) {
          if (message.isDurable()) {
@@ -1269,6 +1267,12 @@ public class ServerSessionImpl implements ServerSession, FailureListener {
       return result;
    }
 
+   protected void checkDefaultAddress(SimpleString address) {
+      if (defaultAddress == null && address != null) {
+         defaultAddress = address;
+      }
+   }
+
    @Override
    public void sendContinuations(final int packetSize,
                                  final long messageBodySize,
@@ -1298,6 +1302,10 @@ public class ServerSessionImpl implements ServerSession, FailureListener {
 
    @Override
    public void requestProducerCredits(final SimpleString address, final int credits) throws Exception {
+      // When the client gets the producer credits it already has the intention of sending messages
+      // and it will already register the default address on the core protocol
+      // hence we need to set it here when we request credits as well
+      checkDefaultAddress(address);
       PagingStore store = server.getPagingManager().getPageStore(address);
 
       if (!store.checkMemory(new Runnable() {
