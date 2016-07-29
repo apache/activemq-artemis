7cf1aae661745b7b86913d895f352a046538ed19
Ville Skyttä <ville.skytta@iki.fi>
2016-07-29 11:40:36 +0300

    Remove unnecessary field override

--- tests/jms-tests/src/test/java/org/apache/activemq/artemis/jms/tests/NonDurableSubscriberTest.java
+++ tests/jms-tests/src/test/java/org/apache/activemq/artemis/jms/tests/NonDurableSubscriberTest.java
@@ -35,8 +35,6 @@ public class NonDurableSubscriberTest extends JMSTestCase {
 
    // Attributes ----------------------------------------------------
 
-   protected InitialContext ic;
-
    // Constructors --------------------------------------------------
 
    // Public --------------------------------------------------------
