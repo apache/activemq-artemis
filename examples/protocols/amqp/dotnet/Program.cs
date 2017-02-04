781e5e64a42f7cbbfac40294a6ce6e33074731ba
Clebert Suconic <clebertsuconic@apache.org>
2017-02-03 21:48:18 -0500

    NO-JIRA: Adding .net example

---
+++ examples/protocols/amqp/dotnet/Program.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Amqp;
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string address = "amqp://a:a@localhost:5672";
+
+            Connection connection = new Connection(new Address(address));
+            Session session = new Session(connection);
+            SenderLink sender = new SenderLink(session, "test-sender", "q1");
+
+            Message message1 = new Message("Hello AMQP!");
+            sender.Send(message1);
+
+
+            Console.WriteLine("Message sent into queue q1");
+        }
+    }
+}
---
+++ examples/protocols/amqp/dotnet/readme.md
@@ -0,0 +1,50 @@
+# Running the .NET AMQP example
+
+
+# Pre-requisites:
+
+All of this can be done on Linux, Mac and... Windows
+
+- Install .NET
+
+https://www.microsoft.com/net/core
+
+
+- Visual Studio Code is free and may be useful:
+
+https://code.visualstudio.com
+
+
+- Powershell might be also useful:
+
+https://github.com/PowerShell/PowerShell/
+
+
+
+# running the example
+
+- Create the broker, by running:
+
+```
+../../../../bin/artemis create ./server1 --user a --password a --role a --allow-anonymous --force
+./server1/bin/artemis-service start
+```
+
+Or simply use the start-broker.sh script on this directory
+
+
+- Compile the code
+
+You need to restore the package, build and run so .NET may download your packages from NuGET:
+
+```sh
+dotnet restore
+dotnet build
+dotnet run
+```
+
+Or simply use the run-example.sh script on this directory
+
+- Debugging
+
+Visual Studio Code will make it fairly easy to do it
