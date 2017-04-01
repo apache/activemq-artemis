apache/activemq-artemis

commit 81b5563a52ab56cbc30e91fcdd274985af13d0d8

ARTEMIS-992 NPE on startup

artemis-server/src/main/java/org/apache/activemq/artemis/core/server/ActiveMQServerLogger.java
@@ -1574,4 +1574,7 @@ public interface ActiveMQServerLogger extends BasicLogger {
    @Message(id = 224075, value = "Cannot find pageTX id = {0}", format = Message.Format.MESSAGE_FORMAT)
    void journalCannotFindPageTX(Long id);
 
+   @LogMessage(level = Logger.Level.ERROR)
+   @Message(id = 224076, value = "No cluster locator found. This server will not participate in a cluster. Ensure a valid cluster-connection is configured.", format = Message.Format.MESSAGE_FORMAT)
+   void noClusterLocatorFound();
 }

artemis-server/src/main/java/org/apache/activemq/artemis/core/server/cluster/ClusterController.java
@@ -106,6 +106,9 @@ public class ClusterController implements ActiveMQComponent {
       } else {
          replicationLocator = defaultLocator;
       }
+      if (replicationLocator == null) {
+         ActiveMQServerLogger.LOGGER.noClusterLocatorFound();
+      }
       //latch so we know once we are connected
       replicationClusterConnectedLatch = new CountDownLatch(1);
       //and add the quorum manager as a topology listener
@@ -198,7 +201,9 @@ public class ClusterController implements ActiveMQComponent {
     * @param listener
     */
    public void addClusterTopologyListenerForReplication(ClusterTopologyListener listener) {
-      replicationLocator.addClusterTopologyListener(listener);
+      if (replicationLocator != null) {
+         replicationLocator.addClusterTopologyListener(listener);
+      }
    }
 
    /**

commit 5f939cf61f43f2be3570527f4238e4ea1b220f08

ARTEMIS-992 Reverting 'ERROR' message
Commit 81b5563a52ab56cbc30e91fcdd274985af13d0d8 introduced an error message
even if you start a regular non clustered server.

artemis-server/src/main/java/org/apache/activemq/artemis/core/server/ActiveMQServerLogger.java
@@ -1573,8 +1573,4 @@ public interface ActiveMQServerLogger extends BasicLogger {
    @LogMessage(level = Logger.Level.ERROR)
    @Message(id = 224075, value = "Cannot find pageTX id = {0}", format = Message.Format.MESSAGE_FORMAT)
    void journalCannotFindPageTX(Long id);
-
-   @LogMessage(level = Logger.Level.ERROR)
-   @Message(id = 224076, value = "No cluster locator found. This server will not participate in a cluster. Ensure a valid cluster-connection is configured.", format = Message.Format.MESSAGE_FORMAT)
-   void noClusterLocatorFound();
 }

artemis-server/src/main/java/org/apache/activemq/artemis/core/server/cluster/ClusterController.java
@@ -106,9 +106,6 @@ public class ClusterController implements ActiveMQComponent {
       } else {
          replicationLocator = defaultLocator;
       }
-      if (replicationLocator == null) {
-         ActiveMQServerLogger.LOGGER.noClusterLocatorFound();
-      }
       //latch so we know once we are connected
       replicationClusterConnectedLatch = new CountDownLatch(1);
       //and add the quorum manager as a topology listener