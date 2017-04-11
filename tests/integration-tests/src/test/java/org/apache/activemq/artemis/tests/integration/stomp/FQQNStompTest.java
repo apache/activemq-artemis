3d85c52e3852c69763194dab6a5ed7ae148efc61
Howard Gao <howard.gao@gmail.com>
2017-04-11 09:20:26 +0800

    ARTEMIS-1093 Full qualified queue name support
    Broker should support full qualified queue names (FQQN)
    as well as bare queue names. This means when clients access
    to a queue they have two equivalent ways to do so. One way
    is by queue names and the other is by FQQN (i.e. address::qname)
    names. Currently only receiving is supported.

---
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/stomp/FQQNStompTest.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.tests.integration.stomp;
+
+import org.apache.activemq.artemis.api.core.SimpleString;
+import org.apache.activemq.artemis.core.server.QueueQueryResult;
+import org.apache.activemq.artemis.tests.integration.stomp.util.ClientStompFrame;
+import org.apache.activemq.artemis.tests.integration.stomp.util.StompClientConnection;
+import org.apache.activemq.artemis.tests.integration.stomp.util.StompClientConnectionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FQQNStompTest extends StompTestBase {
+
+   private StompClientConnection conn;
+
+   @Override
+   @Before
+   public void setUp() throws Exception {
+      super.setUp();
+      conn = StompClientConnectionFactory.createClientConnection("1.2", hostname, port);
+      QueueQueryResult result = server.getActiveMQServer().queueQuery(new SimpleString(getQueueName()));
+      assertTrue(result.isExists());
+      System.out.println("address: " + result.getAddress() + " queu " + result.getName());
+   }
+
+   @Override
+   @After
+   public void tearDown() throws Exception {
+      try {
+         boolean connected = conn != null && conn.isConnected();
+         if (connected) {
+            try {
+               conn.disconnect();
+            } catch (Exception e) {
+            }
+         }
+      } finally {
+         super.tearDown();
+      }
+   }
+
+   @Test
+   //to receive from a FQQN queue like testQueue::testQueue
+   //special care is needed as ":" is a reserved character
+   //in STOMP. Clients need to escape it.
+   public void testReceiveFQQN() throws Exception {
+      conn.connect(defUser, defPass);
+      subscribeQueue(conn, "sub-01", getQueueName() + "\\c\\c" + getQueueName());
+      sendJmsMessage("Hello World!");
+      ClientStompFrame frame = conn.receiveFrame(2000);
+      assertNotNull(frame);
+      assertEquals("Hello World!", frame.getBody());
+      System.out.println("frame: " + frame);
+      unsubscribe(conn, "sub-01");
+   }
+
+}
