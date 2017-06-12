337d250cee7fc5032fe23744cf4c5b2db6199ecd
Andy Taylor <andy.tayls67@gmail.com>
2017-06-12 16:20:43 +0100

    ARTEMIS-1224 - change the journal file size to nearest multiple
    https://issues.apache.org/jira/browse/ARTEMIS-1224

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/persistence/impl/journal/JournalStorageManager.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/persistence/impl/journal/JournalStorageManager.java
@@ -142,7 +142,17 @@ public class JournalStorageManager extends AbstractJournalStorageManager {
 
       journalFF.setDatasync(config.isJournalDatasync());
 
-      Journal localMessage = new JournalImpl(ioExecutors, config.getJournalFileSize(), config.getJournalMinFiles(), config.getJournalPoolFiles(), config.getJournalCompactMinFiles(), config.getJournalCompactPercentage(), journalFF, "activemq-data", "amq", journalFF.getMaxIO(), 0);
+
+      int fileSize = config.getJournalFileSize();
+      // we need to correct the file size if its not a multiple of the alignement
+      if (fileSize % journalFF.getAlignment() != 0) {
+         int difference = fileSize % journalFF.getAlignment();
+         int low = config.getJournalFileSize() - difference;
+         int high = low + journalFF.getAlignment();
+         fileSize = difference < journalFF.getAlignment() / 2 ? low : high;
+         ActiveMQServerLogger.LOGGER.invalidJournalFileSize(config.getJournalFileSize(), fileSize, journalFF.getAlignment());
+      }
+      Journal localMessage = new JournalImpl(ioExecutors, fileSize, config.getJournalMinFiles(), config.getJournalPoolFiles(), config.getJournalCompactMinFiles(), config.getJournalCompactPercentage(), journalFF, "activemq-data", "amq", journalFF.getMaxIO(), 0);
 
       messageJournal = localMessage;
       originalMessageJournal = localMessage;
