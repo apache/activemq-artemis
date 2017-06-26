144a7c8a42654d1723df41d39acbd8b223632a22
Howard Gao <howard.gao@gmail.com>
2017-06-26 16:19:09 +0800

    ARTEMIS-1220 Diverted LargeMessage file corrupted during replication
    When a large message is being diverted, a new copy of the original
    message is created and replicated (if there is a backup) to the backup.
    
    In LargeServerMessageImpl.copy(long) it reuse a byte array to copy
    message body. It is possible that one block of date is read into
    the byte array before the previous read has been replicated,
    causing the replicated bytes to corrupt.
    
    If we make a copy of the byte array before replication, the corruption
    of data will be avoided.
    
    (cherry picked from commit 045021f7df583f6109cb0749dc1601c9a85dbe75)

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/persistence/impl/journal/LargeServerMessageImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/persistence/impl/journal/LargeServerMessageImpl.java
@@ -295,7 +295,10 @@ public final class LargeServerMessageImpl extends ServerMessageImpl implements L
             byte[] bufferToWrite;
             if (bytesRead <= 0) {
                break;
-            } else if (bytesRead == bufferBytes.length) {
+            } else if (bytesRead == bufferBytes.length && !this.storageManager.isReplicated()) {
+               // ARTEMIS-1220: We cannot reuse the same buffer if it's replicated
+               // otherwise there could be another thread still using the buffer on a
+               // replication.
                bufferToWrite = bufferBytes;
             } else {
                bufferToWrite = new byte[bytesRead];
