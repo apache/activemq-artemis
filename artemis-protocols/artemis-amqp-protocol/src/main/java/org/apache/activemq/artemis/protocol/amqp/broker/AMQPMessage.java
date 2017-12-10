01dbf937cdaf1aa3bfc93d41df6785df38275ddb
Todd Baert <toddbaert@gmail.com>
2017-12-09 22:30:16 -0500

    ARTEMIS-1542 - AMQP message cluster-bridging fix
    Fixing ambiguous method call.

--- artemis-protocols/artemis-amqp-protocol/src/main/java/org/apache/activemq/artemis/protocol/amqp/broker/AMQPMessage.java
+++ artemis-protocols/artemis-amqp-protocol/src/main/java/org/apache/activemq/artemis/protocol/amqp/broker/AMQPMessage.java
@@ -611,7 +611,7 @@ public class AMQPMessage extends RefCountMessage {
       if (address != null) {
         return setAddress(address.toString());
       } else {
-        return setAddress(null);
+        return setAddress((String) null);
       }     
    }
 
