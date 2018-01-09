c649bc5b64b57c3541125e0ddd38b479bf9d07ac
Stanislav Knot <sknot@redhat.com>
2018-01-09 17:17:49 +0100

    ARTEMIS-1580 Browsing in hawtio console does not allow to show empty pages

--- artemis-hawtio/artemis-plugin/src/main/webapp/plugin/js/queues.js
+++ artemis-hawtio/artemis-plugin/src/main/webapp/plugin/js/queues.js
@@ -286,6 +286,9 @@ var ARTEMIS = (function(ARTEMIS) {
             if (parseInt(newVal.currentPage) && newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
                 $scope.loadTable();
             }
+            if (parseInt(newVal.pageSize) && newVal !== oldVal && newVal.pageSize !== oldVal.pageSize) {
+                $scope.pagingOptions.currentPage = 1;
+            }
         }, true);
 
         function getBrokerMBean(jolokia) {
