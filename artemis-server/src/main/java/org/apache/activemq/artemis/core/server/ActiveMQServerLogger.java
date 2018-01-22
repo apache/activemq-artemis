f63f15b009d9886023838cbd9bf854a8380f8c8b
Clebert Suconic <clebertsuconic@apache.org>
2018-01-22 12:33:14 -0500

    ARTEMIS-1628 Limit pool size on artemis journal

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/ActiveMQServerLogger.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/ActiveMQServerLogger.java
@@ -1574,6 +1574,10 @@ public interface ActiveMQServerLogger extends BasicLogger {
    @Message(id = 222268, value = "Failed to remove a record", format = Message.Format.MESSAGE_FORMAT)
    void failedToRemoveRecord(@Cause Exception e);
 
+   @LogMessage(level = Logger.Level.WARN)
+   @Message(id = 222269, value = "Please use a fixed value for \"journal-pool-files\". Default changed per https://issues.apache.org/jira/browse/ARTEMIS-1628", format = Message.Format.MESSAGE_FORMAT)
+   void useFixedValueOnJournalPoolFiles();
+
    @LogMessage(level = Logger.Level.ERROR)
    @Message(id = 224000, value = "Failure in initialisation", format = Message.Format.MESSAGE_FORMAT)
    void initializationError(@Cause Throwable e);
