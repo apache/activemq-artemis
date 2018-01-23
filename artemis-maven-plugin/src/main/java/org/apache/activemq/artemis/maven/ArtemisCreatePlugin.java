9e97dcbb8d9aff2c1c36f95abee02e435af6fb74
Justin Bertram <jbertram@apache.org>
2018-01-23 09:51:11 -0600

    ARTEMIS-1414 potential PrintStream leak

--- artemis-maven-plugin/src/main/java/org/apache/activemq/artemis/maven/ArtemisCreatePlugin.java
+++ artemis-maven-plugin/src/main/java/org/apache/activemq/artemis/maven/ArtemisCreatePlugin.java
@@ -233,11 +233,10 @@ public class ArtemisCreatePlugin extends ArtemisAbstractPlugin {
          throw new MojoExecutionException(e.getMessage(), e);
       }
 
-      PrintStream commandLineStream = new PrintStream(outputStream);
-      commandLineStream.println("# These are the commands used to create " + instance.getName());
-      commandLineStream.println(getCommandline(listCommands));
+      try (PrintStream commandLineStream = new PrintStream(outputStream)) {
+         commandLineStream.println("# These are the commands used to create " + instance.getName());
+         commandLineStream.println(getCommandline(listCommands));
 
-      try {
          Artemis.execute(home, null, listCommands);
 
          if (configuration != null) {
@@ -261,8 +260,6 @@ public class ArtemisCreatePlugin extends ArtemisAbstractPlugin {
                copyToLib(file, commandLineStream);
             }
          }
-
-         commandLineStream.close();
 
          FileUtil.makeExec(commandLine);
 
