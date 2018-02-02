apache/activemq-artemis

commit bb84f679363f62e8b2663f63bf23f04133de481d

ARTEMIS-1600 Support masked passwords in bootstrap.xm and login.config
We provide a feature to mask passwords in the configuration files.
However, passwords in the bootstrap.xml (when the console is
secured with HTTPS) cannot be masked. This enhancement has
been opened to allow passwords in the bootstrap.xml to be masked
using the built-in masking feature provided by the broker.

Also the LDAPLoginModule configuration (in login.config) has a
connection password attribute that also needs this mask support.

In addition the ENC() syntax is supported for password masking
to replace the old 'mask-password' flag.

artemis-server/src/main/java/org/apache/activemq/artemis/core/deployers/impl/FileConfigurationParser.java
@@ -80,7 +80,6 @@ import org.apache.activemq.artemis.utils.ByteUtil;
 import org.apache.activemq.artemis.utils.ClassloadingUtil;
 import org.apache.activemq.artemis.utils.DefaultSensitiveStringCodec;
 import org.apache.activemq.artemis.utils.PasswordMaskingUtil;
-import org.apache.activemq.artemis.utils.SensitiveDataCodec;
 import org.apache.activemq.artemis.utils.XMLConfigurationUtil;
 import org.apache.activemq.artemis.utils.XMLUtil;
 import org.apache.activemq.artemis.utils.critical.CriticalAnalyzerPolicy;
@@ -339,7 +338,7 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
 
       config.setManagementNotificationAddress(new SimpleString(getString(e, "management-notification-address", config.getManagementNotificationAddress().toString(), Validators.NOT_NULL_OR_EMPTY)));
 
-      config.setMaskPassword(getBoolean(e, "mask-password", false));
+      config.setMaskPassword(getBoolean(e, "mask-password", null));
 
       config.setPasswordCodec(getString(e, "password-codec", DefaultSensitiveStringCodec.class.getName(), Validators.NOT_NULL_OR_EMPTY));
 
@@ -369,15 +368,11 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
       // parsing cluster password
       String passwordText = getString(e, "cluster-password", null, Validators.NO_CHECK);
 
-      final boolean maskText = config.isMaskPassword();
+      final Boolean maskText = config.isMaskPassword();
 
       if (passwordText != null) {
-         if (maskText) {
-            SensitiveDataCodec<String> codec = PasswordMaskingUtil.getCodec(config.getPasswordCodec());
-            config.setClusterPassword(codec.decode(passwordText));
-         } else {
-            config.setClusterPassword(passwordText);
-         }
+         String resolvedPassword = PasswordMaskingUtil.resolveMask(maskText, passwordText, config.getPasswordCodec());
+         config.setClusterPassword(resolvedPassword);
       }
 
       config.setClusterUser(getString(e, "cluster-user", config.getClusterUser(), Validators.NO_CHECK));
@@ -1164,12 +1159,10 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
 
       Map<String, Object> params = configurations.get(0).getParams();
 
-      if (mainConfig.isMaskPassword()) {
-         params.put(ActiveMQDefaultConfiguration.getPropMaskPassword(), mainConfig.isMaskPassword());
+      params.put(ActiveMQDefaultConfiguration.getPropMaskPassword(), mainConfig.isMaskPassword());
 
-         if (mainConfig.getPasswordCodec() != null) {
-            params.put(ActiveMQDefaultConfiguration.getPropPasswordCodec(), mainConfig.getPasswordCodec());
-         }
+      if (mainConfig.getPasswordCodec() != null) {
+         params.put(ActiveMQDefaultConfiguration.getPropPasswordCodec(), mainConfig.getPasswordCodec());
       }
 
       return configurations.get(0);
@@ -1187,12 +1180,10 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
 
       Map<String, Object> params = configurations.get(0).getParams();
 
-      if (mainConfig.isMaskPassword()) {
-         params.put(ActiveMQDefaultConfiguration.getPropMaskPassword(), mainConfig.isMaskPassword());
+      params.put(ActiveMQDefaultConfiguration.getPropMaskPassword(), mainConfig.isMaskPassword());
 
-         if (mainConfig.getPasswordCodec() != null) {
-            params.put(ActiveMQDefaultConfiguration.getPropPasswordCodec(), mainConfig.getPasswordCodec());
-         }
+      if (mainConfig.getPasswordCodec() != null) {
+         params.put(ActiveMQDefaultConfiguration.getPropPasswordCodec(), mainConfig.getPasswordCodec());
       }
 
       return configurations.get(0);
@@ -1720,9 +1711,6 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
 
       NodeList clusterPassNodes = brNode.getElementsByTagName("password");
       String password = null;
-      boolean maskPassword = mainConfig.isMaskPassword();
-
-      SensitiveDataCodec<String> codec = null;
 
       if (clusterPassNodes.getLength() > 0) {
          Node passNode = clusterPassNodes.item(0);
@@ -1730,10 +1718,7 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
       }
 
       if (password != null) {
-         if (maskPassword) {
-            codec = PasswordMaskingUtil.getCodec(mainConfig.getPasswordCodec());
-            password = codec.decode(password);
-         }
+         password = PasswordMaskingUtil.resolveMask(mainConfig.isMaskPassword(), password, mainConfig.getPasswordCodec());
       } else {
          password = ActiveMQDefaultConfiguration.getDefaultClusterPassword();
       }

artemis-cli/src/test/java/org/apache/activemq/cli/test/HashUtilTest.java
@@ -1,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.cli.test;
+
+import org.apache.activemq.artemis.cli.commands.util.HashUtil;
+import org.apache.activemq.artemis.utils.PasswordMaskingUtil;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class HashUtilTest {
+
+   @Test
+   public void testDefaultHashFormat() throws Exception {
+      final String password = "helloworld";
+      String hash = HashUtil.tryHash(new TestActionContext(), password);
+      String hashStr = PasswordMaskingUtil.unwrap(hash);
+      System.out.println("hashString: " + hashStr);
+      String[] parts = hashStr.split(":");
+      assertEquals(3, parts.length);
+      //first part should be able to convert to an int
+      Integer.parseInt(parts[0]);
+      //second and third parts are all hex values
+      checkHexBytes(parts[1], parts[2]);
+   }
+
+   private void checkHexBytes(String... parts) throws Exception {
+      for (String p : parts) {
+         assertTrue(p.matches("^[0-9A-F]+$"));
+      }
+   }
+}

artemis-commons/src/main/java/org/apache/activemq/artemis/utils/PasswordMaskingUtil.java
@@ -28,14 +28,54 @@ import org.apache.activemq.artemis.logs.ActiveMQUtilBundle;
 
 public final class PasswordMaskingUtil {
 
+   public static final String BEGIN_ENC = "ENC(";
+   public static final String END_ENC = ")";
+
    private PasswordMaskingUtil() {
 
+   }
+
+   /**
+    * This method deals with password masking and returns the password in its plain text form.
+    * @param maskPassword : explicit mask flag. If it's true, the password is interpreted as
+    *                     masked. If it is false, the password is interpreted as plain text.
+    *                     if it is null, the password will be interpreted as masked if the
+    *                     password is wrapped in ENC(), or as plain text otherwise.
+    * @param password : the original value of password string
+    * @param codecClass : the codec used to decode the password. Only when the password is interpreted
+    *              as masked will this codec be used. Ignored otherwise.
+    * @return
+    */
+   public static String resolveMask(Boolean maskPassword, String password, String codecClass) throws Exception {
+      String plainText = password;
+      if (maskPassword == null) {
+         if (isEncMasked(password)) {
+            //masked
+            String bareMaskedPassword = unwrap(password);
+            plainText = getCodec(codecClass).decode(bareMaskedPassword);
+         }
+      } else if (maskPassword) {
+         plainText = getCodec(codecClass).decode(password);
+      }
+      return plainText;
+   }
+
+   public static boolean isEncMasked(String password) {
+      return (password.startsWith(BEGIN_ENC) && password.endsWith(END_ENC));
+   }
+
+   //remove ENC() from the password body
+   public static String unwrap(String password) {
+      return password.substring(4, password.length() - 1);
+   }
+
+   public static String wrap(String password) {
+      return BEGIN_ENC + password + END_ENC;
    }
 
    private static final class LazyPlainTextProcessorHolder {
 
       private LazyPlainTextProcessorHolder() {
-
       }
 
       private static final HashProcessor INSTANCE = new NoHashProcessor();
@@ -83,7 +123,7 @@ public final class PasswordMaskingUtil {
    }
 
    private static boolean isEncoded(String storedPassword) {
-      return storedPassword == null || (storedPassword.startsWith(SecureHashProcessor.BEGIN_HASH) && storedPassword.endsWith(SecureHashProcessor.END_HASH));
+      return storedPassword == null || (isEncMasked(storedPassword));
    }
 
    public static HashProcessor getHashProcessor() {
@@ -106,6 +146,10 @@ public final class PasswordMaskingUtil {
     */
    public static SensitiveDataCodec<String> getCodec(String codecDesc) throws ActiveMQException {
       SensitiveDataCodec<String> codecInstance;
+
+      if (codecDesc == null) {
+         return getDefaultCodec();
+      }
 
       // semi colons
       String[] parts = codecDesc.split(";");

artemis-commons/src/main/java/org/apache/activemq/artemis/utils/SecureHashProcessor.java
@@ -18,9 +18,6 @@ package org.apache.activemq.artemis.utils;
 
 public class SecureHashProcessor implements HashProcessor {
 
-   public static final String BEGIN_HASH = "ENC(";
-   public static final String END_HASH = ")";
-
    private DefaultSensitiveStringCodec codec;
 
    public SecureHashProcessor(DefaultSensitiveStringCodec codec) {
@@ -29,13 +26,13 @@ public class SecureHashProcessor implements HashProcessor {
 
    @Override
    public String hash(String plainText) throws Exception {
-      return BEGIN_HASH + codec.encode(plainText) + END_HASH;
+      return PasswordMaskingUtil.wrap(codec.encode(plainText));
    }
 
    @Override
    //storedValue must take form of ENC(...)
    public boolean compare(char[] inputValue, String storedValue) {
-      String storedHash = storedValue.substring(4, storedValue.length() - 2);
+      String storedHash = storedValue.substring(4, storedValue.length() - 1);
       return codec.verify(inputValue, storedHash);
    }
 }

artemis-commons/src/main/java/org/apache/activemq/artemis/utils/SensitiveDataCodec.java
@@ -31,5 +31,6 @@ public interface SensitiveDataCodec<T> {
 
    T encode(Object secret) throws Exception;
 
-   void init(Map<String, String> params) throws Exception;
+   default void init(Map<String, String> params) throws Exception {
+   };
 }

artemis-commons/src/test/java/org/apache/activemq/artemis/utils/MaskPasswordResolvingTest.java
@@ -1,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.utils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class MaskPasswordResolvingTest {
+
+   private static final String plainPassword = "password";
+   private static final String defaultMaskPassword = "defaultmasked";
+   private static final String customizedCodecPassword = "secret";
+   private static final String oldDefaultMaskedPassword = "oldmasked";
+   private static final String oldCustomizedCodecPassword = "secret";
+   private static final String oldExplicitPlainPassword = "PASSWORD";
+
+   @Parameterized.Parameters(name = "mask({0})password({1})codec({2})")
+   public static Collection<Object[]> params() {
+      return Arrays.asList(new Object[][]{{null, plainPassword, null},
+                                          {null, "ENC(3bdfd94fe8cdf710e7fefa72f809ea90)", null},
+                                          {null, "ENC(momsword)", "org.apache.activemq.artemis.utils.MaskPasswordResolvingTest$SimplePasswordCodec"},
+                                          {true, "662d05f5a83f9e073af6b8dc081d34aa", null},
+                                          {true, "momsword", "org.apache.activemq.artemis.utils.MaskPasswordResolvingTest$SimplePasswordCodec"},
+                                          {false, oldExplicitPlainPassword, null},
+                                          {false, oldExplicitPlainPassword, "org.apache.activemq.artemis.utils.MaskPasswordResolvingTest$SimplePasswordCodec"}});
+   }
+
+   private Boolean maskPassword;
+   private String password;
+   private String codec;
+
+   public MaskPasswordResolvingTest(Boolean maskPassword, String password, String codec) {
+      this.maskPassword = maskPassword;
+      this.password = password;
+      this.codec = codec;
+   }
+
+   @Test
+   public void testPasswordResolving() throws Exception {
+      String resolved = PasswordMaskingUtil.resolveMask(maskPassword, password, codec);
+      System.out.println("resolved: " + resolved);
+      checkResult(resolved);
+   }
+
+   private void checkResult(String resolved) throws Exception {
+      if (this.maskPassword == null) {
+         if (PasswordMaskingUtil.isEncMasked(this.password)) {
+            if (this.codec != null) {
+               assertEquals(customizedCodecPassword, resolved);
+            } else {
+               assertEquals(defaultMaskPassword, resolved);
+            }
+         } else {
+            assertEquals(plainPassword, resolved);
+         }
+      } else {
+         if (this.maskPassword) {
+            if (this.codec != null) {
+               assertEquals(oldCustomizedCodecPassword, resolved);
+            } else {
+               assertEquals(oldDefaultMaskedPassword, resolved);
+            }
+         } else {
+            assertEquals(oldExplicitPlainPassword, resolved);
+         }
+      }
+   }
+
+   public static class SimplePasswordCodec implements SensitiveDataCodec<String> {
+
+      private Map<String, String> passwordBook = new HashMap<>();
+
+      public SimplePasswordCodec() {
+         passwordBook.put("momsword", "secret");
+         passwordBook.put("youneverknow", "keypass");
+         passwordBook.put("youcanguess", "trustpass");
+      }
+
+      @Override
+      public String decode(Object mask) throws Exception {
+         String password = passwordBook.get(mask);
+         if (password == null) {
+            throw new IllegalArgumentException("I don't know the password " + mask);
+         }
+         return password;
+      }
+
+      @Override
+      public String encode(Object secret) throws Exception {
+         return null;
+      }
+   }
+}

artemis-core-client/src/main/java/org/apache/activemq/artemis/api/config/ActiveMQDefaultConfiguration.java
@@ -179,7 +179,7 @@ public final class ActiveMQDefaultConfiguration {
    private static String DEFAULT_CLUSTER_PASSWORD = "CHANGE ME!!";
 
    // This option controls whether passwords in server configuration need be masked. If set to "true" the passwords are masked.
-   private static boolean DEFAULT_MASK_PASSWORD = false;
+   private static Boolean DEFAULT_MASK_PASSWORD = null;
 
    // true means that the management API is available via JMX
    private static boolean DEFAULT_JMX_MANAGEMENT_ENABLED = true;
@@ -622,7 +622,7 @@ public final class ActiveMQDefaultConfiguration {
    /**
     * This option controls whether passwords in server configuration need be masked. If set to "true" the passwords are masked.
     */
-   public static boolean isDefaultMaskPassword() {
+   public static Boolean isDefaultMaskPassword() {
       return DEFAULT_MASK_PASSWORD;
    }
 

artemis-core-client/src/main/java/org/apache/activemq/artemis/core/client/ActiveMQClientMessageBundle.java
@@ -178,9 +178,6 @@ public interface ActiveMQClientMessageBundle {
    @Message(id = 119043, value = "Invalid argument null handler")
    IllegalArgumentException nullHandler();
 
-   @Message(id = 119044, value = "No available codec to decode password!")
-   IllegalArgumentException noCodec();
-
    @Message(id = 119045, value = "the first node to be compared is null")
    IllegalArgumentException firstNodeNull();
 
@@ -213,9 +210,6 @@ public interface ActiveMQClientMessageBundle {
 
    @Message(id = 119055, value = "Element {0} requires a valid Long value, but ''{1}'' cannot be parsed as a Long", format = Message.Format.MESSAGE_FORMAT)
    IllegalArgumentException mustBeLong(Node element, String value);
-
-   @Message(id = 119056, value = "Failed to get decoder")
-   IllegalArgumentException failedToGetDecoder(@Cause Exception e);
 
    @Message(id = 119057, value = "Error decoding password")
    IllegalArgumentException errordecodingPassword(@Cause Exception e);

commit 9acdec2468aa89cf6dfd99323f87c5c1a351bd0d

ARTEMIS-1655 Fix TransportConfiguration encode failing
Check for null on isMaskPassword, seems this regressed in bb84f679363f62e8b2663f63bf23f04133de481d change for ARTEMIS-1600

artemis-server/src/main/java/org/apache/activemq/artemis/core/deployers/impl/FileConfigurationParser.java
@@ -1159,7 +1159,9 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
 
       Map<String, Object> params = configurations.get(0).getParams();
 
-      params.put(ActiveMQDefaultConfiguration.getPropMaskPassword(), mainConfig.isMaskPassword());
+      if (mainConfig.isMaskPassword() != null) {
+         params.put(ActiveMQDefaultConfiguration.getPropMaskPassword(), mainConfig.isMaskPassword());
+      }
 
       if (mainConfig.getPasswordCodec() != null) {
          params.put(ActiveMQDefaultConfiguration.getPropPasswordCodec(), mainConfig.getPasswordCodec());
@@ -1180,7 +1182,9 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
 
       Map<String, Object> params = configurations.get(0).getParams();
 
-      params.put(ActiveMQDefaultConfiguration.getPropMaskPassword(), mainConfig.isMaskPassword());
+      if (mainConfig.isMaskPassword() != null) {
+         params.put(ActiveMQDefaultConfiguration.getPropMaskPassword(), mainConfig.isMaskPassword());
+      }
 
       if (mainConfig.getPasswordCodec() != null) {
          params.put(ActiveMQDefaultConfiguration.getPropPasswordCodec(), mainConfig.getPasswordCodec());