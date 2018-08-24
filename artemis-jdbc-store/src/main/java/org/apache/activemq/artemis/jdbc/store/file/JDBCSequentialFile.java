d5c81d573302c62a2f308d9ca887e3bd70bd7312
Martyn Taylor <mtaylor@redhat.com>
2018-08-24 14:24:25 +0100

    ARTEMIS-2056 Set write position on JDBCFile copy

--- artemis-jdbc-store/src/main/java/org/apache/activemq/artemis/jdbc/store/file/JDBCSequentialFile.java
+++ artemis-jdbc-store/src/main/java/org/apache/activemq/artemis/jdbc/store/file/JDBCSequentialFile.java
@@ -330,6 +330,7 @@ public class JDBCSequentialFile implements SequentialFile {
    public SequentialFile cloneFile() {
       try {
          JDBCSequentialFile clone = new JDBCSequentialFile(fileFactory, filename, executor, dbDriver, writeLock);
+         clone.setWritePosition(this.writePosition);
          return clone;
       } catch (Exception e) {
          fileFactory.onIOError(e, "Error cloning JDBC file.", this);
@@ -342,8 +343,13 @@ public class JDBCSequentialFile implements SequentialFile {
       JDBCSequentialFile clone = (JDBCSequentialFile) cloneFile;
       try {
          synchronized (writeLock) {
+            if (logger.isTraceEnabled()) {
+               logger.trace("JDBC Copying File.  From: " + this + " To: " + cloneFile);
+            }
+
             clone.open();
             dbDriver.copyFileData(this, clone);
+            clone.setWritePosition(writePosition);
          }
       } catch (Exception e) {
          fileFactory.onIOError(e, "Error copying JDBC file.", this);
