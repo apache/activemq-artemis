a15a35c6f6b5820186f48db266b45ecc07829a8a
Clebert Suconic <clebertsuconic@apache.org>
2019-01-14 16:11:29 -0500

    ARTEMIS-2228 Large Messages over Management

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/management/impl/AbstractControl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/management/impl/AbstractControl.java
@@ -21,8 +21,19 @@ import javax.management.MBeanInfo;
 import javax.management.MBeanOperationInfo;
 import javax.management.NotCompliantMBeanException;
 import javax.management.StandardMBean;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.Map;
 
+import org.apache.activemq.artemis.api.core.Message;
+import org.apache.activemq.artemis.api.core.SimpleString;
+import org.apache.activemq.artemis.core.message.impl.CoreMessage;
 import org.apache.activemq.artemis.core.persistence.StorageManager;
+import org.apache.activemq.artemis.core.persistence.impl.journal.DummyOperationContext;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ServerSession;
+import org.apache.activemq.artemis.utils.Base64;
+import org.apache.activemq.artemis.utils.UUIDGenerator;
 
 public abstract class AbstractControl extends StandardMBean {
 
@@ -77,8 +88,61 @@ public abstract class AbstractControl extends StandardMBean {
       return new MBeanInfo(info.getClassName(), info.getDescription(), fillMBeanAttributeInfo(), info.getConstructors(), fillMBeanOperationInfo(), info.getNotifications());
    }
 
-   // Private -------------------------------------------------------
+   protected String sendMessage(SimpleString address,
+                                ActiveMQServer server,
+                                Map<String, String> headers,
+                                int type,
+                                String body,
+                                boolean durable,
+                                String user,
+                                String password,
+                                Long...queueID) throws Exception {
+      ManagementRemotingConnection fakeConnection = new ManagementRemotingConnection();
+      ServerSession serverSession = server.createSession("management::" + UUIDGenerator.getInstance().generateStringUUID(), user, password,
+                                                         Integer.MAX_VALUE, fakeConnection,
+                                                         true, true, false,
+                                                         false, address.toString(), fakeConnection.callback,
+                                                         false, new DummyOperationContext(), Collections.emptyMap());
+      try {
+         CoreMessage message = new CoreMessage(storageManager.generateID(), 50);
+         if (headers != null) {
+            for (String header : headers.keySet()) {
+               message.putStringProperty(new SimpleString(header), new SimpleString(headers.get(header)));
+            }
+         }
+         message.setType((byte) type);
+         message.setDurable(durable);
+         message.setTimestamp(System.currentTimeMillis());
+         if (body != null) {
+            if (type == Message.TEXT_TYPE) {
+               message.getBodyBuffer().writeNullableSimpleString(new SimpleString(body));
+            } else {
+               message.getBodyBuffer().writeBytes(Base64.decode(body));
+            }
+         }
 
-   // Inner classes -------------------------------------------------
+         message.setAddress(address);
+
+         // if a queueID is used, we set the routeToIDs property
+         // to one or many specific queues
+         if (queueID != null && queueID.length > 0) {
+            ByteBuffer buffer = ByteBuffer.allocate(8 * queueID.length);
+            for (Long q : queueID) {
+               buffer.putLong(q);
+            }
+            message.putBytesProperty(Message.HDR_ROUTE_TO_IDS, buffer.array());
+         }
+
+         // There's no point on direct delivery using the management thread, use false here
+         serverSession.send(message, false);
+         return "" + message.getMessageID();
+      } finally {
+         try {
+            serverSession.close(false);
+         } catch (Exception ignored) {
+         }
+      }
+   }
+   // Inner classes------------------------------------------------
 
 }
