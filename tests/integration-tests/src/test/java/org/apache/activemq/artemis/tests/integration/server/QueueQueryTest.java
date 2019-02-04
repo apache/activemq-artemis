545c52a87ee8578dbe860adb666ba4f549076dca
Justin Bertram <jbertram@apache.org>
2019-02-04 16:01:37 -0600

    NO-JIRA fix race condition in QueueQueryTest
    My previous commit to fix this was amended which nullified the fix.

--- tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/server/QueueQueryTest.java
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/server/QueueQueryTest.java
@@ -161,14 +161,14 @@ public class QueueQueryTest extends ActiveMQTestBase {
          QueueQueryResult queueQueryResult = server.queueQuery(fqqn);
          assertEquals(queueName, queueQueryResult.getName());
          assertEquals(addressName, queueQueryResult.getAddress());
-         Wait.assertEquals(1, server.queueQuery(fqqn)::getMessageCount);
+         Wait.assertEquals(1, () -> server.queueQuery(fqqn).getMessageCount());
          queueQueryResult = server.queueQuery(queueName);
          assertEquals(queueName, queueQueryResult.getName());
          assertEquals(addressName, queueQueryResult.getAddress());
          assertEquals(1, queueQueryResult.getMessageCount());
          c.createProducer().send(c.createQueue(addressName.toString()), c.createMessage());
-         Wait.assertEquals(2, server.queueQuery(fqqn)::getMessageCount);
-         Wait.assertEquals(2, server.queueQuery(queueName)::getMessageCount);
+         Wait.assertEquals(2, () -> server.queueQuery(fqqn).getMessageCount());
+         Wait.assertEquals(2, () -> server.queueQuery(queueName).getMessageCount());
       }
    }
 
