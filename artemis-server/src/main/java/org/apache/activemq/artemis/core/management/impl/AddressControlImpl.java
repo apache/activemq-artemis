bee9512955123ffd0148fe24cd5df4cc0d0db379
Clebert Suconic <clebertsuconic@apache.org>
2019-03-12 20:21:03 -0400

    NO-JIRA fixing another test
    test failing: org.apache.activemq.artemis.tests.integration.management.AddressControlTest.testSendMessage

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/management/impl/AddressControlImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/management/impl/AddressControlImpl.java
@@ -307,6 +307,7 @@ public class AddressControlImpl extends AbstractControl implements AddressContro
       try {
          return sendMessage(addressInfo.getName(), server, headers, type, body, durable, user, password);
       } catch (Exception e) {
+         e.printStackTrace();
          throw new IllegalStateException(e.getMessage());
       }
    }
