9872128a1fc7dd1721ef1cbcbd0221564bc523d5
Clebert Suconic <clebertsuconic@apache.org>
2019-05-02 12:24:19 -0400

    NO-JIRA Fixing GlobalPagingTest

--- tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/paging/GlobalPagingTest.java
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/paging/GlobalPagingTest.java
@@ -330,13 +330,15 @@ public class GlobalPagingTest extends PagingTest {
             try (ClientProducer requestProducer = session.createProducer(managementAddress)) {
                final SimpleString replyQueue = new SimpleString(managementAddress + "." + UUID.randomUUID().toString());
                session.createTemporaryQueue(replyQueue, ActiveMQDefaultConfiguration.getDefaultRoutingType(), replyQueue);
+               int id = 1000;
                try (ClientConsumer consumer = session.createConsumer(replyQueue)) {
                   final Queue queue = server.locateQueue(replyQueue);
                   final MessageReference reference = MessageReference.Factory.createReference(session.createMessage(false), queue);
+                  reference.getMessage().setMessageID(id++);
                   //it will cause QueueImpl::directDeliver -> false
                   queue.addHead(reference, false);
                   Wait.assertFalse(queue::isDirectDeliver);
-                  Wait.assertTrue(() -> queue.removeReferenceWithID(reference.getMessageID()) != null);
+                  queue.removeReferenceWithID(reference.getMessageID());
                   ClientMessage message = session.createMessage(false);
                   message.putStringProperty(ClientMessageImpl.REPLYTO_HEADER_NAME, replyQueue);
                   ManagementHelper.putAttribute(message, "queue." + address.toString(), "messageCount");
