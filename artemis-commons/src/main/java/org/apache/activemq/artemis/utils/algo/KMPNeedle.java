228882c2be63d7ab7c0a273d3220fdd53bb07a42
Francesco Nigro <nigro.fra@gmail.com>
2020-02-09 12:54:14 +0100

    ARTEMIS-2617 Lazy scan AMQP message data

---
+++ artemis-commons/src/main/java/org/apache/activemq/artemis/utils/algo/KMPNeedle.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.utils.algo;
+
+import java.util.Objects;
+
+/**
+ * Abstraction of {@code byte[] }<a href="https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm">Knuth-Morris-Pratt</a>'s needle to be used
+ * to perform pattern matching over indexed haystack of {@code byte}s.
+ */
+public final class KMPNeedle {
+
+   @FunctionalInterface
+   public interface IndexedByteSupplier<S> {
+
+      byte get(S source, int index);
+   }
+
+   private final int[] jumpTable;
+   private final byte[] needle;
+
+   private KMPNeedle(byte[] needle) {
+      Objects.requireNonNull(needle);
+      this.needle = needle;
+      this.jumpTable = createJumpTable(needle);
+   }
+
+   private static int[] createJumpTable(byte[] needle) {
+      final int[] jumpTable = new int[needle.length + 1];
+      int j = 0;
+      for (int i = 1; i < needle.length; i++) {
+         while (j > 0 && needle[j] != needle[i]) {
+            j = jumpTable[j];
+         }
+         if (needle[j] == needle[i]) {
+            j++;
+         }
+         jumpTable[i + 1] = j;
+      }
+      for (int i = 1; i < jumpTable.length; i++) {
+         if (jumpTable[i] != 0) {
+            return jumpTable;
+         }
+      }
+      // optimization over the original algorithm: it would save from accessing any jump table
+      return null;
+   }
+
+   /**
+    * https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm search algorithm:
+    *
+    * This version differ from the original algorithm, because allows to fail fast (and faster) if
+    * the remaining haystack to be processed is < of the remaining needle to be matched.
+    */
+   public <H> int searchInto(IndexedByteSupplier<? super H> haystackReader, H haystack, int end, int start) {
+      assert end >= 0 && start >= 0 && end >= start;
+      final int length = end - start;
+      int j = 0;
+      final int needleLength = needle.length;
+      int remainingNeedle = needleLength;
+      for (int i = 0; i < length; i++) {
+         final int remainingHayStack = length - i;
+         if (remainingNeedle > remainingHayStack) {
+            return -1;
+         }
+         final int index = start + i;
+         final byte value = haystackReader.get(haystack, index);
+         while (j > 0 && needle[j] != value) {
+            j = jumpTable == null ? 0 : jumpTable[j];
+            remainingNeedle = needleLength - j;
+         }
+         if (needle[j] == value) {
+            j++;
+            remainingNeedle--;
+            assert remainingNeedle >= 0;
+         }
+         if (j == needleLength) {
+            final int startMatch = index - needleLength + 1;
+            return startMatch;
+         }
+      }
+      return -1;
+   }
+
+   public static KMPNeedle of(byte[] needle) {
+      return new KMPNeedle(needle);
+   }
+
+}
---
+++ artemis-protocols/artemis-amqp-protocol/src/main/java/org/apache/activemq/artemis/protocol/amqp/broker/AMQPMessageSymbolSearch.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.protocol.amqp.broker;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.IdentityHashMap;
+import java.util.List;
+
+import org.apache.activemq.artemis.protocol.amqp.util.TLSEncode;
+import org.apache.activemq.artemis.utils.algo.KMPNeedle;
+import org.apache.qpid.proton.amqp.Symbol;
+import org.apache.qpid.proton.amqp.messaging.AmqpSequence;
+import org.apache.qpid.proton.amqp.messaging.AmqpValue;
+import org.apache.qpid.proton.amqp.messaging.ApplicationProperties;
+import org.apache.qpid.proton.amqp.messaging.Data;
+import org.apache.qpid.proton.amqp.messaging.Footer;
+import org.apache.qpid.proton.amqp.messaging.MessageAnnotations;
+import org.apache.qpid.proton.amqp.messaging.Properties;
+import org.apache.qpid.proton.codec.DecoderImpl;
+import org.apache.qpid.proton.codec.ReadableBuffer;
+import org.apache.qpid.proton.codec.TypeConstructor;
+
+final class AMQPMessageSymbolSearch {
+
+   // used to quick search for MessageAnnotations
+   private static final IdentityHashMap<Class<?>, Boolean> MSG_BODY_TYPES;
+
+   static {
+      // we're including MessageAnnotations here because it will still cause termination
+      final List<Class<?>> classList = Arrays.asList(MessageAnnotations.class, Properties.class,
+                                                     ApplicationProperties.class, Data.class,
+                                                     AmqpSequence.class, AmqpValue.class, Footer.class);
+      MSG_BODY_TYPES = new IdentityHashMap<>(classList.size());
+      classList.forEach(clazz -> MSG_BODY_TYPES.put(clazz, Boolean.TRUE));
+   }
+
+   public static KMPNeedle kmpNeedleOf(Symbol symbol) {
+      return KMPNeedle.of(symbol.toString().getBytes(StandardCharsets.US_ASCII));
+   }
+
+   public static boolean anyMessageAnnotations(ReadableBuffer data, KMPNeedle[] needles) {
+      DecoderImpl decoder = TLSEncode.getDecoder();
+      final int position = data.position();
+      decoder.setBuffer(data.rewind());
+      try {
+         while (data.hasRemaining()) {
+            TypeConstructor<?> constructor = decoder.readConstructor();
+            final Class<?> typeClass = constructor.getTypeClass();
+            if (MSG_BODY_TYPES.containsKey(typeClass)) {
+               if (MessageAnnotations.class.equals(typeClass)) {
+                  final int start = data.position();
+                  constructor.skipValue();
+                  final int end = data.position();
+                  for (int i = 0, count = needles.length; i < count; i++) {
+                     final int foundIndex = needles[i].searchInto(ReadableBuffer::get, data, end, start);
+                     if (foundIndex != -1) {
+                        return true;
+                     }
+                  }
+               }
+               return false;
+            }
+            constructor.skipValue();
+         }
+         return false;
+      } finally {
+         decoder.setBuffer(null);
+         data.position(position);
+      }
+   }
+
+}
