044d6101e94f338907f90651387041f10aad9896
Ingo Karkat <ingo.karkat@microfocus.com>
2020-12-16 13:57:48 +0100

    ARTEMIS-3081 Swap precedence of key/tr.store props
    If an application wants to use a special key/truststore for Artemis but
    have the remainder of the application use the default Java store, the
    org.apache.activemq.ssl.keyStore needs to take precedence over Java's
    javax.net.ssl.keyStore. However, the current implementation takes the
    first non-null value from
      System.getProperty(JAVAX_KEYSTORE_PATH_PROP_NAME),
      System.getProperty(ACTIVEMQ_KEYSTORE_PATH_PROP_NAME),
      keyStorePath
    
    So if the default Java property is set, no override is possible. Swap
    the order of the JAVAX_... and ACTIVEMQ_... property names so that the
    ActiveMQ ones come first (as a component-specific overrides), the
    standard Java ones comes second, and finally a local attribute value
    (through Stream.of(...).firstFirst()).
    
    (In our case the application uses the default Java truststore location
    at $JAVA_HOME/lib/security/jssecacerts, and only supplies its password
    in javax.net.ssl.trustStorePassword, and then uses a dedicated
    truststore for Artemis. Defining both org.apache.activemq.ssl.trustStore
    and org.apache.activemq.ssl.trustStorePassword now makes Artemis use the
    dedicated truststore (javax.net.ssl.trustStore is not set as we use the
    default location, so the second choice
    org.apache.activemq.ssl.trustStore applies), but with the Java default
    truststore password (first choice javax.net.ssl.trustStorePassword
    applies instead of the second choice because it is set for the default
    truststore). Obviously, this does not work unless both passwords are
    identical!)

--- artemis-core-client/src/main/java/org/apache/activemq/artemis/core/remoting/impl/netty/NettyConnector.java
+++ artemis-core-client/src/main/java/org/apache/activemq/artemis/core/remoting/impl/netty/NettyConnector.java
@@ -575,11 +575,11 @@ public class NettyConnector extends AbstractConnector {
             realTrustStoreProvider = trustStoreProvider;
             realTrustStorePassword = trustStorePassword;
          } else {
-            realKeyStorePath = Stream.of(System.getProperty(JAVAX_KEYSTORE_PATH_PROP_NAME), System.getProperty(ACTIVEMQ_KEYSTORE_PATH_PROP_NAME), keyStorePath).map(v -> useDefaultSslContext ? keyStorePath : v).filter(Objects::nonNull).findFirst().orElse(null);
-            realKeyStorePassword = Stream.of(System.getProperty(JAVAX_KEYSTORE_PASSWORD_PROP_NAME), System.getProperty(ACTIVEMQ_KEYSTORE_PASSWORD_PROP_NAME), keyStorePassword).map(v -> useDefaultSslContext ? keyStorePassword : v).filter(Objects::nonNull).findFirst().orElse(null);
+            realKeyStorePath = Stream.of(System.getProperty(ACTIVEMQ_KEYSTORE_PATH_PROP_NAME), System.getProperty(JAVAX_KEYSTORE_PATH_PROP_NAME), keyStorePath).map(v -> useDefaultSslContext ? keyStorePath : v).filter(Objects::nonNull).findFirst().orElse(null);
+            realKeyStorePassword = Stream.of(System.getProperty(ACTIVEMQ_KEYSTORE_PASSWORD_PROP_NAME), System.getProperty(JAVAX_KEYSTORE_PASSWORD_PROP_NAME), keyStorePassword).map(v -> useDefaultSslContext ? keyStorePassword : v).filter(Objects::nonNull).findFirst().orElse(null);
             realKeyStoreProvider = Stream.of(System.getProperty(ACTIVEMQ_KEYSTORE_PROVIDER_PROP_NAME), keyStoreProvider).map(v -> useDefaultSslContext ? keyStoreProvider : v).filter(Objects::nonNull).findFirst().orElse(null);
-            realTrustStorePath = Stream.of(System.getProperty(JAVAX_TRUSTSTORE_PATH_PROP_NAME), System.getProperty(ACTIVEMQ_TRUSTSTORE_PATH_PROP_NAME), trustStorePath).map(v -> useDefaultSslContext ? trustStorePath : v).filter(Objects::nonNull).findFirst().orElse(null);
-            realTrustStorePassword = Stream.of(System.getProperty(JAVAX_TRUSTSTORE_PASSWORD_PROP_NAME), System.getProperty(ACTIVEMQ_TRUSTSTORE_PASSWORD_PROP_NAME), trustStorePassword).map(v -> useDefaultSslContext ? trustStorePassword : v).filter(Objects::nonNull).findFirst().orElse(null);
+            realTrustStorePath = Stream.of(System.getProperty(ACTIVEMQ_TRUSTSTORE_PATH_PROP_NAME), System.getProperty(JAVAX_TRUSTSTORE_PATH_PROP_NAME), trustStorePath).map(v -> useDefaultSslContext ? trustStorePath : v).filter(Objects::nonNull).findFirst().orElse(null);
+            realTrustStorePassword = Stream.of(System.getProperty(ACTIVEMQ_TRUSTSTORE_PASSWORD_PROP_NAME), System.getProperty(JAVAX_TRUSTSTORE_PASSWORD_PROP_NAME), trustStorePassword).map(v -> useDefaultSslContext ? trustStorePassword : v).filter(Objects::nonNull).findFirst().orElse(null);
             realTrustStoreProvider = Stream.of(System.getProperty(ACTIVEMQ_TRUSTSTORE_PROVIDER_PROP_NAME), trustStoreProvider).map(v -> useDefaultSslContext ? trustStoreProvider : v).filter(Objects::nonNull).findFirst().orElse(null);
          }
       } else {
