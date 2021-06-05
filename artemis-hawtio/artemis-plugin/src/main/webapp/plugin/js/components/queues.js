686a61dd656e4b40160575a50acab68e770d6426
Andy Taylor <andy.tayls67@gmail.com>
2021-06-05 08:38:02 +0100

    ARTEMIS-3332 - fix search on consumer count in console
    https://issues.apache.org/jira/browse/ARTEMIS-3332

--- artemis-hawtio/artemis-plugin/src/main/webapp/plugin/js/components/queues.js
+++ artemis-hawtio/artemis-plugin/src/main/webapp/plugin/js/components/queues.js
@@ -127,7 +127,7 @@ var Artemis;
             fieldOptions: [
                 {id: 'id', name: 'ID'},
                 {id: 'name', name: 'Name'},
-                {id: 'consumerId', name: 'Consumer ID'},
+                {id: 'consumerCount', name: 'Consumer Count'},
                 {id: 'address', name: 'Address'},
                 {id: 'filter', name: 'Filter'},
                 {id: 'maxConsumers', name: 'Max Consumers'},
