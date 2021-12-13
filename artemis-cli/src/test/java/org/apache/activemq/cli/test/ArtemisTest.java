947f63d02a357c569d625904a07530c0d5affcf7
Justin Bertram <jbertram@apache.org>
2021-12-12 22:53:31 -0600

    ARTEMIS-3671 fix readability of queue stat output

--- artemis-cli/src/test/java/org/apache/activemq/cli/test/ArtemisTest.java
+++ artemis-cli/src/test/java/org/apache/activemq/cli/test/ArtemisTest.java
@@ -1738,6 +1738,80 @@ public class ArtemisTest extends CliTestBase {
    }
 
    @Test
+   public void testQstatColumnWidth() throws Exception {
+
+      File instanceQstat = new File(temporaryFolder.getRoot(), "instanceQStat");
+      setupAuth(instanceQstat);
+      Run.setEmbedded(true);
+      Artemis.main("create", instanceQstat.getAbsolutePath(), "--silent", "--no-fsync", "--no-autotune", "--no-web", "--require-login");
+      System.setProperty("artemis.instance", instanceQstat.getAbsolutePath());
+      Artemis.internalExecute("run");
+
+      try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory("tcp://localhost:61616"); Connection connection = cf.createConnection("admin", "admin");) {
+
+         //set up some queues with messages and consumers
+         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+         connection.start();
+         final String NAME = "012345678901234567890123456789";
+         sendMessages(session, NAME, 1);
+
+         TestActionContext context = new TestActionContext();
+         StatQueue statQueue = new StatQueue();
+         statQueue.setUser("admin");
+         statQueue.setPassword("admin");
+         statQueue.setQueueName(NAME);
+         statQueue.execute(context);
+         ArrayList<String> lines = getOutputLines(context, false);
+         Assert.assertEquals("rows returned", 2, lines.size());
+         String[] split = lines.get(1).split("\\|");
+         Assert.assertEquals(StatQueue.DEFAULT_MAX_COLUMN_SIZE, split[1].length());
+
+         context = new TestActionContext();
+         statQueue = new StatQueue();
+         statQueue.setUser("admin");
+         statQueue.setPassword("admin");
+         statQueue.setQueueName(NAME);
+         statQueue.setMaxColumnSize(15);
+         statQueue.execute(context);
+         lines = getOutputLines(context, false);
+         Assert.assertEquals("rows returned", 2, lines.size());
+         split = lines.get(1).split("\\|");
+         Assert.assertEquals(15, split[1].length());
+
+         context = new TestActionContext();
+         statQueue = new StatQueue();
+         statQueue.setUser("admin");
+         statQueue.setPassword("admin");
+         statQueue.setQueueName(NAME);
+         statQueue.setMaxColumnSize(50);
+         statQueue.execute(context);
+         lines = getOutputLines(context, false);
+         Assert.assertEquals("rows returned", 2, lines.size());
+         split = lines.get(1).split("\\|");
+         Assert.assertEquals(NAME.length(), split[1].length());
+
+         context = new TestActionContext();
+         statQueue = new StatQueue();
+         statQueue.setUser("admin");
+         statQueue.setPassword("admin");
+         statQueue.setQueueName(NAME);
+         statQueue.setMaxColumnSize(-1);
+         statQueue.execute(context);
+         lines = getOutputLines(context, false);
+         for (String line : lines) {
+            System.out.println(line);
+         }
+         Assert.assertEquals("rows returned", 2, lines.size());
+         split = lines.get(1).split("\\|");
+         Assert.assertEquals(NAME.length(), split[1].length());
+         Assert.assertEquals("CONSUMER_COUNT".length(), split[3].length());
+      } finally {
+         stopServer();
+      }
+
+   }
+
+   @Test
    public void testQstatErrors() throws Exception {
 
       File instanceQstat = new File(temporaryFolder.getRoot(), "instanceQStatErrors");
