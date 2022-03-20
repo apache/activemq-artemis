e427e2c2c1b179fde325369720f7664bd1601099
waldi5001 <6708838+waldi5001@users.noreply.github.com>
2022-03-20 13:17:05 +0100

    ARTEMIS-3707 add javaee-8 wlp resource adapter sample

---
+++ examples/features/sub-modules/wlp/src/main/java/org/apache/activemq/artemis/example/wlp/sample/RestEndpoint.java
@@ -0,0 +1,64 @@
+package org.apache.activemq.artemis.example.wlp.sample;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.jms.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import javax.naming.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@Stateless
+@Path("/")
+public class RestEndpoint {
+
+    @Resource(name = "jms/sampleConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Resource(name = "jms/sampleQueue")
+    private Queue queue;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String sendMessage() {
+        Connection con = null;
+		try {
+			con = connectionFactory.createConnection();
+			Session session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			MessageProducer producer = session.createProducer(queue);
+            producer.send(session.createTextMessage("Sample Message"));
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+        return "sent message";
+    }
+
+    private static void listContext(Context ctx, String indent) throws NamingException {
+        NamingEnumeration list = ctx.listBindings("");
+        while (list.hasMore()) {
+            Binding item = (Binding) list.next();
+            String className = item.getClassName();
+            String name = item.getName();
+            System.out.println(name + ": " + className);
+            Object o = item.getObject();
+            if (o instanceof javax.naming.Context) {
+                listContext((Context) o, indent + " ");
+            }
+        }
+    }
+}
