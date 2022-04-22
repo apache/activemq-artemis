83afe0e276078d239ad6505ea391ee48350498f6
Marco Roehrs <marco.roehrs@twinformatics.at>
2022-04-22 15:35:27 +0200

    ARTEMIS-3798 Allow binary deserialization of message properties AMQ_MSG_MESSAGE_ID, AMQ_MSG_ORIG_DESTINATION, AMQ_MSG_PRODUCER_ID & AMQ_MSG_REPLY_TO from existing messages.
    co-authored: Justin Bertram contributed OpenWireMessageConverterTest changes

--- artemis-protocols/artemis-openwire-protocol/src/test/java/org/apache/activemq/artemis/core/protocol/openwire/OpenWireMessageConverterTest.java
+++ artemis-protocols/artemis-openwire-protocol/src/test/java/org/apache/activemq/artemis/core/protocol/openwire/OpenWireMessageConverterTest.java
@@ -25,6 +25,7 @@ import org.apache.activemq.artemis.core.server.MessageReference;
 import org.apache.activemq.artemis.core.server.Queue;
 import org.apache.activemq.artemis.core.server.impl.MessageReferenceImpl;
 import org.apache.activemq.artemis.reader.MessageUtil;
+import org.apache.activemq.artemis.utils.RandomUtil;
 import org.apache.activemq.artemis.utils.UUID;
 import org.apache.activemq.artemis.utils.UUIDGenerator;
 import org.apache.activemq.command.ActiveMQDestination;
@@ -34,10 +35,15 @@ import org.apache.activemq.command.MessageDispatch;
 import org.apache.activemq.command.MessageId;
 import org.apache.activemq.command.ProducerId;
 import org.apache.activemq.openwire.OpenWireFormatFactory;
+import org.apache.activemq.util.ByteSequence;
 import org.apache.activemq.wireformat.WireFormat;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.apache.activemq.artemis.core.protocol.openwire.OpenWireMessageConverter.AMQ_MSG_MESSAGE_ID;
+import static org.apache.activemq.artemis.core.protocol.openwire.OpenWireMessageConverter.AMQ_MSG_ORIG_DESTINATION;
+import static org.apache.activemq.artemis.core.protocol.openwire.OpenWireMessageConverter.AMQ_MSG_PRODUCER_ID;
+import static org.apache.activemq.artemis.core.protocol.openwire.OpenWireMessageConverter.AMQ_MSG_REPLY_TO;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
@@ -151,19 +157,87 @@ public class OpenWireMessageConverterTest {
    }
 
    @Test
+   public void testLegacyProducerId() throws Exception {
+      final String PRODUCER_ID = "123:456:789";
+
+      ActiveMQMessage classicMessage = new ActiveMQMessage();
+      classicMessage.setProducerId(new ProducerId(PRODUCER_ID));
+      final ByteSequence pidBytes = openWireFormat.marshal(classicMessage.getProducerId());
+      pidBytes.compact();
+      ICoreMessage coreMessage = new CoreMessage().initBuffer(8);
+      coreMessage.putBytesProperty(AMQ_MSG_PRODUCER_ID, pidBytes.data);
+      MessageReference messageReference = new MessageReferenceImpl(coreMessage, Mockito.mock(Queue.class));
+      AMQConsumer amqConsumer = Mockito.mock(AMQConsumer.class);
+      Mockito.when(amqConsumer.getOpenwireDestination()).thenReturn(destination);
+      MessageDispatch messageDispatch = OpenWireMessageConverter.createMessageDispatch(messageReference, coreMessage, openWireFormat, amqConsumer, nodeUUID);
+      assertEquals(PRODUCER_ID, messageDispatch.getMessage().getProducerId().toString());
+   }
+
+   @Test
    public void testMessageId() throws Exception {
       final String MESSAGE_ID = "ID:123:456:789";
 
       ActiveMQMessage classicMessage = new ActiveMQMessage();
       classicMessage.setMessageId(new MessageId(MESSAGE_ID));
       Message artemisMessage = OpenWireMessageConverter.inbound(classicMessage.getMessage(), openWireFormat, null);
-      assertEquals(MESSAGE_ID, artemisMessage.getStringProperty(OpenWireMessageConverter.AMQ_MSG_MESSAGE_ID));
+      assertEquals(MESSAGE_ID, artemisMessage.getStringProperty(AMQ_MSG_MESSAGE_ID));
 
       MessageReference messageReference = new MessageReferenceImpl(artemisMessage, Mockito.mock(Queue.class));
       AMQConsumer amqConsumer = Mockito.mock(AMQConsumer.class);
       Mockito.when(amqConsumer.getOpenwireDestination()).thenReturn(destination);
       MessageDispatch classicMessageDispatch = OpenWireMessageConverter.createMessageDispatch(messageReference, (ICoreMessage) artemisMessage, openWireFormat, amqConsumer, nodeUUID);
       assertEquals(MESSAGE_ID, classicMessageDispatch.getMessage().getMessageId().toString());
+   }
+
+   @Test
+   public void testLegacyMessageId() throws Exception {
+      final String MESSAGE_ID = "ID:123:456:789";
+
+      ActiveMQMessage classicMessage = new ActiveMQMessage();
+      classicMessage.setMessageId(new MessageId(MESSAGE_ID));
+      final ByteSequence midBytes = openWireFormat.marshal(classicMessage.getMessageId());
+      midBytes.compact();
+      ICoreMessage coreMessage = new CoreMessage().initBuffer(8);
+      coreMessage.putBytesProperty(AMQ_MSG_MESSAGE_ID, midBytes.data);
+      MessageReference messageReference = new MessageReferenceImpl(coreMessage, Mockito.mock(Queue.class));
+      AMQConsumer amqConsumer = Mockito.mock(AMQConsumer.class);
+      Mockito.when(amqConsumer.getOpenwireDestination()).thenReturn(destination);
+      MessageDispatch messageDispatch = OpenWireMessageConverter.createMessageDispatch(messageReference, coreMessage, openWireFormat, amqConsumer, nodeUUID);
+      assertEquals(MESSAGE_ID, messageDispatch.getMessage().getMessageId().toString());
+   }
+
+   @Test
+   public void testLegacyOriginalDestination() throws Exception {
+      final String DESTINATION = RandomUtil.randomString().replace("-", "");
+
+      ActiveMQMessage classicMessage = new ActiveMQMessage();
+      classicMessage.setOriginalDestination(new ActiveMQQueue(DESTINATION));
+      final ByteSequence destBytes = openWireFormat.marshal(classicMessage.getOriginalDestination());
+      destBytes.compact();
+      ICoreMessage coreMessage = new CoreMessage().initBuffer(8);
+      coreMessage.putBytesProperty(AMQ_MSG_ORIG_DESTINATION, destBytes.data);
+      MessageReference messageReference = new MessageReferenceImpl(coreMessage, Mockito.mock(Queue.class));
+      AMQConsumer amqConsumer = Mockito.mock(AMQConsumer.class);
+      Mockito.when(amqConsumer.getOpenwireDestination()).thenReturn(destination);
+      MessageDispatch messageDispatch = OpenWireMessageConverter.createMessageDispatch(messageReference, coreMessage, openWireFormat, amqConsumer, nodeUUID);
+      assertEquals("queue://" + DESTINATION, messageDispatch.getMessage().getOriginalDestination().toString());
+   }
+
+   @Test
+   public void testLegacyReplyTo() throws Exception {
+      final String DESTINATION = RandomUtil.randomString().replace("-", "");
+
+      ActiveMQMessage classicMessage = new ActiveMQMessage();
+      classicMessage.setJMSReplyTo(new ActiveMQQueue(DESTINATION));
+      final ByteSequence destBytes = openWireFormat.marshal(classicMessage.getJMSReplyTo());
+      destBytes.compact();
+      ICoreMessage coreMessage = new CoreMessage().initBuffer(8);
+      coreMessage.putBytesProperty(AMQ_MSG_REPLY_TO, destBytes.data);
+      MessageReference messageReference = new MessageReferenceImpl(coreMessage, Mockito.mock(Queue.class));
+      AMQConsumer amqConsumer = Mockito.mock(AMQConsumer.class);
+      Mockito.when(amqConsumer.getOpenwireDestination()).thenReturn(destination);
+      MessageDispatch messageDispatch = OpenWireMessageConverter.createMessageDispatch(messageReference, coreMessage, openWireFormat, amqConsumer, nodeUUID);
+      assertEquals("queue://" + DESTINATION, messageDispatch.getMessage().getReplyTo().toString());
    }
 
    @Test
