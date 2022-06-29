clebertsuconic/amq-activemq-artemis

ARTEMIS-3743 / ARTEMIS-3766 Use ACKReason on Mirror to determine target operations and fixing Delivering statistics on Mirror
I merged these two JIRAs into one as I was doing an overal check on Mirroring

(cherry picked from commit a701fb7123e89a5861ff11a5bc378ed2e70e6f36)

artemis-server/src/test/java/org/apache/activemq/artemis/core/server/impl/RoutingContextTest.java
@@ -441,7 +441,8 @@ public class RoutingContextTest {
       public void acknowledge(Transaction tx,
                               MessageReference ref,
                               AckReason reason,
-                              ServerConsumer consumer) throws Exception {
+                              ServerConsumer consumer,
+                              boolean decDelivering) throws Exception {
 
       }
 
@@ -661,7 +662,7 @@ public class RoutingContextTest {
       }
 
       @Override
-      public void expire(MessageReference ref, ServerConsumer consumer) throws Exception {
+      public void expire(MessageReference ref, ServerConsumer consumer, boolean decDelivering) throws Exception {
 
       }
 
@@ -896,6 +897,11 @@ public class RoutingContextTest {
 
       @Override
       public void postAcknowledge(MessageReference ref, AckReason reason) {
+
+      }
+
+      @Override
+      public void postAcknowledge(MessageReference ref, AckReason reason, boolean decDelivering) {
 
       }
 

artemis-server/src/test/java/org/apache/activemq/artemis/core/server/impl/ScheduledDeliveryHandlerTest.java
@@ -1203,7 +1203,7 @@ public class ScheduledDeliveryHandlerTest extends Assert {
       }
 
       @Override
-      public void acknowledge(Transaction tx, MessageReference ref, AckReason reason, ServerConsumer consumer) throws Exception {
+      public void acknowledge(Transaction tx, MessageReference ref, AckReason reason, ServerConsumer consumer, boolean decDelivering) throws Exception {
 
       }
 
@@ -1408,7 +1408,7 @@ public class ScheduledDeliveryHandlerTest extends Assert {
       }
 
       @Override
-      public void expire(MessageReference ref, ServerConsumer consumer) throws Exception {
+      public void expire(MessageReference ref, ServerConsumer consumer, boolean decDelivering) throws Exception {
 
       }
 
@@ -1615,6 +1615,11 @@ public class ScheduledDeliveryHandlerTest extends Assert {
 
       @Override
       public void routeWithAck(Message message, RoutingContext context) {
+
+      }
+
+      @Override
+      public void postAcknowledge(MessageReference ref, AckReason reason, boolean decDelivering) {
 
       }
 

tests/unit-tests/src/test/java/org/apache/activemq/artemis/tests/unit/core/postoffice/impl/FakeQueue.java
@@ -394,7 +394,7 @@ public class FakeQueue extends CriticalComponentImpl implements Queue {
    }
 
    @Override
-   public void acknowledge(Transaction tx, MessageReference ref, AckReason reason, ServerConsumer consumer) throws Exception {
+   public void acknowledge(Transaction tx, MessageReference ref, AckReason reason, ServerConsumer consumer, boolean decDel) throws Exception {
       // no-op
 
    }
@@ -490,7 +490,7 @@ public class FakeQueue extends CriticalComponentImpl implements Queue {
    }
 
    @Override
-   public void expire(final MessageReference ref, final ServerConsumer consumer) throws Exception {
+   public void expire(final MessageReference ref, final ServerConsumer consumer, boolean decDel) throws Exception {
       // no-op
 
    }
@@ -951,6 +951,10 @@ public class FakeQueue extends CriticalComponentImpl implements Queue {
    public LinkedListIterator<MessageReference> browserIterator() {
       // TODO Auto-generated method stub
       return null;
+   }
+
+   @Override
+   public void postAcknowledge(MessageReference ref, AckReason reason, boolean decDelivering) {
    }
 
    @Override

apache/activemq-artemis

artemis-server/src/test/java/org/apache/activemq/artemis/core/server/impl/RoutingContextTest.java
@@ -441,7 +441,8 @@ public class RoutingContextTest {
       public void acknowledge(Transaction tx,
                               MessageReference ref,
                               AckReason reason,
-                              ServerConsumer consumer) throws Exception {
+                              ServerConsumer consumer,
+                              boolean delivering) throws Exception {
 
       }
 
@@ -661,7 +662,7 @@ public class RoutingContextTest {
       }
 
       @Override
-      public void expire(MessageReference ref, ServerConsumer consumer) throws Exception {
+      public void expire(MessageReference ref, ServerConsumer consumer, boolean delivering) throws Exception {
 
       }
 
@@ -896,6 +897,11 @@ public class RoutingContextTest {
 
       @Override
       public void postAcknowledge(MessageReference ref, AckReason reason) {
+
+      }
+
+      @Override
+      public void postAcknowledge(MessageReference ref, AckReason reason, boolean delivering) {
 
       }
 

artemis-server/src/test/java/org/apache/activemq/artemis/core/server/impl/ScheduledDeliveryHandlerTest.java
@@ -1213,7 +1213,7 @@ public class ScheduledDeliveryHandlerTest extends Assert {
       }
 
       @Override
-      public void acknowledge(Transaction tx, MessageReference ref, AckReason reason, ServerConsumer consumer) throws Exception {
+      public void acknowledge(Transaction tx, MessageReference ref, AckReason reason, ServerConsumer consumer, boolean delivering) throws Exception {
 
       }
 
@@ -1418,7 +1418,7 @@ public class ScheduledDeliveryHandlerTest extends Assert {
       }
 
       @Override
-      public void expire(MessageReference ref, ServerConsumer consumer) throws Exception {
+      public void expire(MessageReference ref, ServerConsumer consumer, boolean delivering) throws Exception {
 
       }
 
@@ -1630,6 +1630,11 @@ public class ScheduledDeliveryHandlerTest extends Assert {
 
       @Override
       public void routeWithAck(Message message, RoutingContext context) {
+
+      }
+
+      @Override
+      public void postAcknowledge(MessageReference ref, AckReason reason, boolean delivering) {
 
       }
 

tests/unit-tests/src/test/java/org/apache/activemq/artemis/tests/unit/core/postoffice/impl/FakeQueue.java
@@ -396,7 +396,7 @@ public class FakeQueue extends CriticalComponentImpl implements Queue {
    }
 
    @Override
-   public void acknowledge(Transaction tx, MessageReference ref, AckReason reason, ServerConsumer consumer) throws Exception {
+   public void acknowledge(Transaction tx, MessageReference ref, AckReason reason, ServerConsumer consumer, boolean decDel) throws Exception {
       // no-op
 
    }
@@ -492,7 +492,7 @@ public class FakeQueue extends CriticalComponentImpl implements Queue {
    }
 
    @Override
-   public void expire(final MessageReference ref, final ServerConsumer consumer) throws Exception {
+   public void expire(final MessageReference ref, final ServerConsumer consumer, boolean decDel) throws Exception {
       // no-op
 
    }
@@ -964,6 +964,10 @@ public class FakeQueue extends CriticalComponentImpl implements Queue {
    public LinkedListIterator<MessageReference> browserIterator() {
       // TODO Auto-generated method stub
       return null;
+   }
+
+   @Override
+   public void postAcknowledge(MessageReference ref, AckReason reason, boolean delivering) {
    }
 
    @Override