aef789641d1a55d5251f2e16dd67268e2d53a038
eidottermihi <eidottermihi@gmail.com>
2022-08-18 09:05:06 +0200

    ARTEMIS-2876 Add JUnit5 Extensions for testing
    Adds a new module 'artemis-junit-5' which adds JUnit 5 Extensions for
    unit testing. For backwards compability, 'artemis-junit' still uses
    JUnit 4. Common stuff has been moved to 'artemis-junit-commons'. Work is
    based on the initial PR
    https://github.com/apache/activemq-artemis/pull/3436 by @luisalves00

---
+++ artemis-junit/artemis-junit-5/src/test/java/org/apache/activemq/artemis/junit/MultipleEmbeddedActiveMQResourcesTest.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.junit;
+
+import org.apache.activemq.artemis.api.core.SimpleString;
+import org.apache.activemq.artemis.api.core.client.ClientMessage;
+import org.apache.activemq.artemis.core.settings.impl.AddressSettings;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class MultipleEmbeddedActiveMQResourcesTest {
+
+   static final SimpleString TEST_QUEUE_ONE = new SimpleString("test.queue.one");
+   static final SimpleString TEST_QUEUE_TWO = new SimpleString("test.queue.two");
+   static final SimpleString TEST_ADDRESS_ONE = new SimpleString("test.address.one");
+   static final SimpleString TEST_ADDRESS_TWO = new SimpleString("test.address.two");
+
+   static final String TEST_BODY = "Test Message";
+
+   static final String ASSERT_SENT_FORMAT = "Message should have been sent to %s";
+   static final String ASSERT_RECEIVED_FORMAT = "Message should have been received from %s";
+
+   @RegisterExtension
+   @Order(1)
+   public EmbeddedActiveMQExtension serverOne = new EmbeddedActiveMQExtension(1);
+
+   @RegisterExtension
+   @Order(2)
+   public EmbeddedActiveMQExtension serverTwo = new EmbeddedActiveMQExtension(2);
+
+   @BeforeAll
+   public void setUp() throws Exception {
+      serverOne.getServer().getActiveMQServer().getAddressSettingsRepository().addMatch("#", new AddressSettings().setDeadLetterAddress(SimpleString.toSimpleString("DLA")).setExpiryAddress(SimpleString.toSimpleString("Expiry")));
+      serverTwo.getServer().getActiveMQServer().getAddressSettingsRepository().addMatch("#", new AddressSettings().setDeadLetterAddress(SimpleString.toSimpleString("DLA")).setExpiryAddress(SimpleString.toSimpleString("Expiry")));
+
+      serverOne.createQueue(TEST_ADDRESS_ONE, TEST_QUEUE_ONE);
+      serverTwo.createQueue(TEST_ADDRESS_TWO, TEST_QUEUE_TWO);
+   }
+
+   @Test
+   public void testMultipleServers() throws Exception {
+      ClientMessage sentOne = serverOne.sendMessage(TEST_ADDRESS_ONE, TEST_BODY);
+      assertNotNull(sentOne, String.format(ASSERT_SENT_FORMAT, TEST_QUEUE_ONE));
+
+      ClientMessage receivedOne = serverOne.receiveMessage(TEST_QUEUE_ONE);
+      assertNotNull(receivedOne, String.format(ASSERT_RECEIVED_FORMAT, TEST_QUEUE_TWO));
+
+      ClientMessage sentTwo = serverTwo.sendMessage(TEST_ADDRESS_TWO, TEST_BODY);
+      assertNotNull(sentOne, String.format(ASSERT_SENT_FORMAT, TEST_QUEUE_TWO));
+
+      ClientMessage receivedTwo = serverTwo.receiveMessage(TEST_QUEUE_TWO);
+      assertNotNull(receivedOne, String.format(ASSERT_RECEIVED_FORMAT, TEST_QUEUE_TWO));
+   }
+
+}
---
+++ artemis-junit/artemis-junit-commons/src/main/java/org/apache/activemq/artemis/junit/ActiveMQProducerOperations.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.junit;
+
+import java.util.Map;
+
+import org.apache.activemq.artemis.api.core.client.ClientMessage;
+
+public interface ActiveMQProducerOperations {
+
+   boolean isUseDurableMessage();
+
+   /**
+    * Disables/Enables creating durable messages. By default, durable messages are created.
+    * @param useDurableMessage if true, durable messages will be created
+    */
+   void setUseDurableMessage(boolean useDurableMessage);
+
+   /**
+    * Create a ClientMessage.
+    * <p>
+    * If useDurableMessage is false, a non-durable message is created. Otherwise, a durable message
+    * is created.
+    * @return a new ClientMessage
+    */
+   ClientMessage createMessage();
+
+   /**
+    * Create a ClientMessage with the specified body.
+    * <p>
+    * If useDurableMessage is false, a non-durable message is created. Otherwise, a durable message
+    * is created.
+    * @param body the body for the new message
+    * @return a new ClientMessage with the specified body
+    */
+   ClientMessage createMessage(byte[] body);
+
+   /**
+    * Create a ClientMessage with the specified body.
+    * <p>
+    * If useDurableMessage is false, a non-durable message is created. Otherwise, a durable message
+    * is created.
+    * @param body the body for the new message
+    * @return a new ClientMessage with the specified body
+    */
+   ClientMessage createMessage(String body);
+
+   /**
+    * Create a ClientMessage with the specified message properties.
+    * <p>
+    * If useDurableMessage is false, a non-durable message is created. Otherwise, a durable message
+    * is created.
+    * @param properties message properties for the new message
+    * @return a new ClientMessage with the specified message properties
+    */
+   ClientMessage createMessage(Map<String, Object> properties);
+
+   /**
+    * Create a ClientMessage with the specified body and message properties.
+    * <p>
+    * If useDurableMessage is false, a non-durable message is created. Otherwise, a durable message
+    * is created.
+    * @param body the body for the new message
+    * @param properties message properties for the new message
+    * @return a new ClientMessage with the specified body and message properties
+    */
+   ClientMessage createMessage(byte[] body, Map<String, Object> properties);
+
+   /**
+    * Create a ClientMessage with the specified body and message properties.
+    * <p>
+    * If useDurableMessage is false, a non-durable message is created. Otherwise, a durable message
+    * is created.
+    * @param body the body for the new message
+    * @param properties message properties for the new message
+    * @return a new ClientMessage with the specified body and message properties
+    */
+   ClientMessage createMessage(String body, Map<String, Object> properties);
+
+   /**
+    * Send a ClientMessage to the server.
+    * @param message the message to send
+    */
+   void sendMessage(ClientMessage message);
+
+   /**
+    * Create a new ClientMessage with the specified body and send to the server.
+    * @param body the body for the new message
+    * @return the message that was sent
+    */
+   ClientMessage sendMessage(byte[] body);
+
+   /**
+    * Create a new ClientMessage with the specified body and send to the server.
+    * @param body the body for the new message
+    * @return the message that was sent
+    */
+   ClientMessage sendMessage(String body);
+
+   /**
+    * Create a new ClientMessage with the specified properties and send to the server
+    * @param properties the properties for the new message
+    * @return the message that was sent
+    */
+   ClientMessage sendMessage(Map<String, Object> properties);
+
+   /**
+    * Create a new ClientMessage with the specified body and and properties and send to the server
+    * @param properties the properties for the new message
+    * @return the message that was sent
+    */
+   ClientMessage sendMessage(byte[] body, Map<String, Object> properties);
+
+   /**
+    * Create a new ClientMessage with the specified body and and properties and send to the server
+    * @param properties the properties for the new message
+    * @return the message that was sent
+    */
+   ClientMessage sendMessage(String body, Map<String, Object> properties);
+
+}
