apache/activemq-artemis

commit f632e8104bbdae1fbf3658fec47e180784e957da

ARTEMIS-3833 Preserve JMSCorrelationID of distributed AMQP large messages

artemis-protocols/artemis-amqp-protocol/src/main/java/org/apache/activemq/artemis/protocol/amqp/broker/AMQPMessage.java
@@ -245,13 +245,17 @@ public abstract class AMQPMessage extends RefCountMessage implements org.apache.
 
       this.headerPosition = copy.headerPosition;
       this.encodedHeaderSize = copy.encodedHeaderSize;
+      this.header = copy.header == null ? null : new Header(copy.header);
       this.deliveryAnnotationsPosition = copy.deliveryAnnotationsPosition;
       this.encodedDeliveryAnnotationsSize = copy.encodedDeliveryAnnotationsSize;
+      this.deliveryAnnotations = copy.deliveryAnnotations == null ? null : new DeliveryAnnotations(copy.deliveryAnnotations.getValue());
       this.messageAnnotationsPosition = copy.messageAnnotationsPosition;
+      this.messageAnnotations = copy.messageAnnotations == null ? null : new MessageAnnotations(copy.messageAnnotations.getValue());
       this.propertiesPosition = copy.propertiesPosition;
+      this.properties = copy.properties == null ? null : new Properties(copy.properties);
       this.applicationPropertiesPosition = copy.applicationPropertiesPosition;
+      this.applicationProperties = copy.applicationProperties == null ? null : new ApplicationProperties(copy.applicationProperties.getValue());
       this.remainingBodyPosition = copy.remainingBodyPosition;
-      this.applicationProperties = copy.applicationProperties;
       this.messageDataScanned = copy.messageDataScanned;
    }
 

tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/cluster/distribution/AMQPLargeMessageClusterTest.java
@@ -21,12 +21,14 @@ import javax.jms.BytesMessage;
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Consumer;
 
 import org.apache.activemq.artemis.api.core.RoutingType;
 import org.apache.activemq.artemis.core.server.cluster.impl.MessageLoadBalancingType;
@@ -80,6 +82,29 @@ public class AMQPLargeMessageClusterTest extends ClusterTestBase {
 
    @Test(timeout = RECEIVE_TIMEOUT_MILLIS * (MESSAGES + 1))
    public void testSendReceiveLargeMessage() throws Exception {
+      testSendReceiveLargeMessage(message -> { }, message -> { });
+   }
+
+   @Test(timeout = RECEIVE_TIMEOUT_MILLIS * (MESSAGES + 1))
+   public void testSendReceiveLargeMessageWithJMSCorrelationID() throws Exception {
+      final String jmsCorrelationID = "123456";
+      testSendReceiveLargeMessage(message -> {
+         try {
+            message.setJMSCorrelationID(jmsCorrelationID);
+         } catch (JMSException e) {
+            fail("Exception not expected: " + e);
+         }
+      }, message -> {
+         try {
+            Assert.assertEquals(jmsCorrelationID, message.getJMSCorrelationID());
+         } catch (JMSException e) {
+            fail("Exception not expected: " + e);
+         }
+      });
+   }
+
+
+   private void testSendReceiveLargeMessage(Consumer<Message> beforeSending, Consumer<Message> afterReceiving) throws Exception {
       setupCluster(MessageLoadBalancingType.ON_DEMAND);
 
       startServers(0, 1);
@@ -99,12 +124,16 @@ public class AMQPLargeMessageClusterTest extends ClusterTestBase {
 
       String producerUri = "amqp://localhost:61616";
       final JmsConnectionFactory producerFactory = new JmsConnectionFactory(producerUri);
-      try (Connection producerConnection = producerFactory.createConnection(); Session producerSession = producerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
+      try (Connection producerConnection = producerFactory.createConnection();
+           Session producerSession = producerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
          producerConnection.start();
          final Destination queue = producerSession.createQueue(queueName);
          String consumerUri = "amqp://localhost:61617";
          final JmsConnectionFactory consumerConnectionFactory = new JmsConnectionFactory(consumerUri);
-         try (Connection consumerConnection = consumerConnectionFactory.createConnection(); Session consumerSession = consumerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE); MessageConsumer consumer = consumerSession.createConsumer(queue); MessageProducer producer = producerSession.createProducer(queue)) {
+         try (Connection consumerConnection = consumerConnectionFactory.createConnection();
+              Session consumerSession = consumerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+              MessageConsumer consumer = consumerSession.createConsumer(queue);
+              MessageProducer producer = producerSession.createProducer(queue)) {
             producer.setDeliveryMode(DeliveryMode.PERSISTENT);
             consumerConnection.start();
             final byte[] largeMessageContent = new byte[MESSAGE_SIZE];
@@ -113,6 +142,7 @@ public class AMQPLargeMessageClusterTest extends ClusterTestBase {
             for (int i = 0; i < MESSAGES; i++) {
                final BytesMessage sentMessage = producerSession.createBytesMessage();
                sentMessage.writeBytes(largeMessageContent);
+               beforeSending.accept(sentMessage);
                producer.send(sentMessage);
                final Message receivedMessage = consumer.receive(RECEIVE_TIMEOUT_MILLIS);
                Assert.assertNotNull("A message should be received in " + RECEIVE_TIMEOUT_MILLIS + " ms", receivedMessage);
@@ -124,6 +154,7 @@ public class AMQPLargeMessageClusterTest extends ClusterTestBase {
                   e.printStackTrace();
                   System.exit(-1);
                }
+               afterReceiving.accept(receivedMessage);
             }
          }
       }

commit 41a24e66265b685eb1934f89eb6ead71601807b1

ARTEMIS-4268 AMQPMessage copy constructor shouldn't copy all message annotations
During redistribution, we should not copy all message annotations.

In particular we should not copy any of the x-opt-ORIG annotations used on DLQ and other copies.

this was broken after f632e8104bbdae1fbf3658fec47e180784e957da (ARTEMIS-3833 Preserve JMSCorrelationID of distributed AMQP large messages)

The change preserved too much, and as a result of that AmqpLargeMessageRedistributionTest::testSendMessageToBroker0GetFromBroker2 is intermittently failing.

There is no test in this commit as this is fixing AmqpLargeMessageRedistributionTest

artemis-protocols/artemis-amqp-protocol/src/main/java/org/apache/activemq/artemis/protocol/amqp/broker/AMQPMessage.java
@@ -252,13 +252,27 @@ public abstract class AMQPMessage extends RefCountMessage implements org.apache.
       this.encodedDeliveryAnnotationsSize = copy.encodedDeliveryAnnotationsSize;
       this.deliveryAnnotations = copy.deliveryAnnotations == null ? null : new DeliveryAnnotations(copy.deliveryAnnotations.getValue());
       this.messageAnnotationsPosition = copy.messageAnnotationsPosition;
-      this.messageAnnotations = copy.messageAnnotations == null ? null : new MessageAnnotations(copy.messageAnnotations.getValue());
+      this.messageAnnotations = copyAnnotations(copy.messageAnnotations);
       this.propertiesPosition = copy.propertiesPosition;
       this.properties = copy.properties == null ? null : new Properties(copy.properties);
       this.applicationPropertiesPosition = copy.applicationPropertiesPosition;
       this.applicationProperties = copy.applicationProperties == null ? null : new ApplicationProperties(copy.applicationProperties.getValue());
       this.remainingBodyPosition = copy.remainingBodyPosition;
       this.messageDataScanned = copy.messageDataScanned;
+   }
+
+   private static MessageAnnotations copyAnnotations(MessageAnnotations messageAnnotations) {
+      if (messageAnnotations == null) {
+         return null;
+      }
+      HashMap newAnnotation = new HashMap();
+      messageAnnotations.getValue().forEach((a, b) -> {
+         // These properties should not be copied when re-routing the messages
+         if (!a.toString().startsWith("x-opt-ORIG") && !a.toString().equals("x-opt-routing-type")) {
+            newAnnotation.put(a, b);
+         }
+      });
+      return new MessageAnnotations(newAnnotation);
    }
 
    protected AMQPMessage(long messageFormat) {