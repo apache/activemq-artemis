0c8adee37a59ffb9276e20ca34f62a19af5da6bc
Clebert Suconic <clebertsuconic@apache.org>
2023-05-23 12:48:26 -0400

    ARTEMIS-4290 Separating some integration tests into legacy-integration-tests
    legacy-integration-tests is being created to hold LDAP Tests (or any other tests that won't play well with keeping threads clean)
    it will have fork-mode=always on the maven-surefire-plugin

--- tests/integration-tests-isolated/src/test/java/org/apache/activemq/artemis/tests/integration/isolated/security/LegacyLDAPSecuritySettingPluginRefreshTest.java
+++ tests/integration-tests-isolated/src/test/java/org/apache/activemq/artemis/tests/integration/isolated/security/LegacyLDAPSecuritySettingPluginRefreshTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.activemq.artemis.tests.integration.security;
+package org.apache.activemq.artemis.tests.integration.isolated.security;
 
 import java.util.Map;
 
