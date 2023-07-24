486f5d92a6cd3bf8aacbedaf188595806e0b9883
Justin Bertram <jbertram@apache.org>
2023-07-24 09:41:40 -0500

    NO-JIRA test

--- tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/mqtt5/MQTT5Test.java
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/mqtt5/MQTT5Test.java
@@ -14,6 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+ * Test
+ */
 package org.apache.activemq.artemis.tests.integration.mqtt5;
 
 import javax.jms.JMSConsumer;
