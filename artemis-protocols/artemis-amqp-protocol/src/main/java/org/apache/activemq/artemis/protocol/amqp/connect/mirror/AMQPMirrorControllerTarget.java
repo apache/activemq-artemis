47dcafe3e863cc8d52ab41d6468daf8359c09454
Clebert Suconic <clebertsuconic@apache.org>
2024-01-11 11:05:20 -0500

    ARTEMIS-4564 Cleanup large message files in case of mirror duplicate detection

--- artemis-protocols/artemis-amqp-protocol/src/main/java/org/apache/activemq/artemis/protocol/amqp/connect/mirror/AMQPMirrorControllerTarget.java
+++ artemis-protocols/artemis-amqp-protocol/src/main/java/org/apache/activemq/artemis/protocol/amqp/connect/mirror/AMQPMirrorControllerTarget.java
@@ -504,6 +504,7 @@ public class AMQPMirrorControllerTarget extends ProtonAbstractReceiver implement
       byte[] duplicateIDBytes = ByteUtil.longToBytes(internalIDLong);
 
       if (duplicateIDCache.contains(duplicateIDBytes)) {
+         message.usageDown(); // large messages would be removed here
          flow();
          return false;
       }
