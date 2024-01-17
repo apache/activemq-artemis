1197898232dff8f724cd8dd6a22c1bb88e58b572
Justin Bertram <jbertram@apache.org>
2024-01-17 15:00:15 -0600

    ARTEMIS-4571 race condition w/TTL impacting in-vm connections
    There is a race condition between ConnectionEntry.ttl and
    FailureCheckAndFlushThread whereby an in-vm connection may get closed
    inadvertently due to a TTL timeout. This is because ConnectionEntry.ttl
    is initialized to 60000 and then later set to -1 upon the initial Ping.
    If this update happens at *just* the right time in
    FailureCheckAndFlushThread then the connection will be closed.
    
    The fix ensures that the ConnectionEntry.ttl is set to -1 for in-vm
    connections from the start. It also eliminates the possibility of the
    race in FailureCheckAndFlushThread.
    
    This fix is based on static analysis of the code. The timing window is
    just too small to contruct a reliable test. The failure has only been
    seen in the wild a handful of times.

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/remoting/server/impl/RemotingServiceImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/remoting/server/impl/RemotingServiceImpl.java
@@ -762,19 +762,19 @@ public class RemotingServiceImpl implements RemotingService, ServerConnectionLif
       public void run() {
          while (!closed) {
             try {
-               long now = System.currentTimeMillis();
-
                Set<Pair<Object, Long>> toRemove = new HashSet<>();
-
                for (ConnectionEntry entry : connections.values()) {
                   final RemotingConnection conn = entry.connection;
+                  final long lastCheck = entry.lastCheck;
+                  final long ttl = entry.ttl;
+                  final long now = System.currentTimeMillis();
 
                   boolean flush = true;
 
-                  if (entry.ttl != -1) {
+                  if (ttl != -1) {
                      if (!conn.checkDataReceived()) {
-                        if (now >= entry.lastCheck + entry.ttl) {
-                           toRemove.add(new Pair<>(conn.getID(), entry.ttl));
+                        if (now >= lastCheck + ttl) {
+                           toRemove.add(new Pair<>(conn.getID(), ttl));
 
                            flush = false;
                         }
