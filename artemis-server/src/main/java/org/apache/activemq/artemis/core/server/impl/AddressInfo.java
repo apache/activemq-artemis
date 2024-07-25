096a869c1ed6102a43074434db07899c70231c4a
Howard Gao <howard.gao@gmail.com>
2024-07-25 22:50:14 +0800

    ARTEMIS-4954 AddressControl.pause() can pause the snf queue

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/AddressInfo.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/AddressInfo.java
@@ -16,6 +16,7 @@
  */
 package org.apache.activemq.artemis.core.server.impl;
 
+import org.apache.activemq.artemis.core.postoffice.impl.LocalQueueBinding;
 import org.apache.activemq.artemis.json.JsonArray;
 import org.apache.activemq.artemis.json.JsonArrayBuilder;
 import org.apache.activemq.artemis.json.JsonNumber;
@@ -35,7 +36,6 @@ import org.apache.activemq.artemis.core.persistence.StorageManager;
 import org.apache.activemq.artemis.core.postoffice.Binding;
 import org.apache.activemq.artemis.core.postoffice.Bindings;
 import org.apache.activemq.artemis.core.postoffice.PostOffice;
-import org.apache.activemq.artemis.core.postoffice.QueueBinding;
 import org.apache.activemq.artemis.core.server.ActiveMQServerLogger;
 import org.apache.activemq.artemis.core.settings.HierarchicalRepositoryChangeListener;
 import org.apache.activemq.artemis.utils.CompositeAddress;
@@ -216,8 +216,8 @@ public class AddressInfo {
          Bindings bindings = postOffice.lookupBindingsForAddress(this.getName());
          if (bindings != null) {
             for (Binding binding : bindings.getBindings()) {
-               if (binding instanceof QueueBinding) {
-                  ((QueueBinding) binding).getQueue().pause(false);
+               if (binding instanceof LocalQueueBinding) {
+                  ((LocalQueueBinding) binding).getQueue().pause(false);
                }
             }
          }
@@ -250,8 +250,8 @@ public class AddressInfo {
          Bindings bindings = postOffice.lookupBindingsForAddress(this.getName());
          if (bindings != null) {
             for (Binding binding : bindings.getBindings()) {
-               if (binding instanceof QueueBinding) {
-                  ((QueueBinding) binding).getQueue().pause(false);
+               if (binding instanceof LocalQueueBinding) {
+                  ((LocalQueueBinding) binding).getQueue().pause(false);
                }
             }
          }
@@ -278,8 +278,8 @@ public class AddressInfo {
          Bindings bindings = postOffice.lookupBindingsForAddress(this.getName());
          if (bindings != null) {
             for (Binding binding : bindings.getBindings()) {
-               if (binding instanceof QueueBinding) {
-                  ((QueueBinding) binding).getQueue().resume();
+               if (binding instanceof LocalQueueBinding) {
+                  ((LocalQueueBinding) binding).getQueue().resume();
                }
             }
          }
