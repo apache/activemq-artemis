6eed8e232b69102812a7154547b1ec307057e1be
Howard Gao <howard.gao@gmail.com>
2024-08-18 22:24:46 +0800

    ARTEMIS-5002 AMQP producer not unblock if the disk space is freed

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/paging/impl/PagingStoreImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/paging/impl/PagingStoreImpl.java
@@ -1096,6 +1096,7 @@ public class PagingStoreImpl implements PagingStore {
          if (isFull()) {
             if (runOnFailure && runWhenAvailable != null) {
                addToBlockList(runWhenAvailable, blockedCallback);
+               pagingManager.addBlockedStore(this);
             }
             return false;
          }
@@ -1178,7 +1179,7 @@ public class PagingStoreImpl implements PagingStore {
          }
       }
 
-      return false;
+      return !blocking;
    }
 
    @Override
