1f90b5be0d0d1c3e699499251b3cbfb31317508d
Howard Gao <howard.gao@gmail.com>
2024-09-24 11:51:30 +0800

    ARTEMIS-5062 ClusterConnectionControl has wrong annotation

--- artemis-core-client/src/main/java/org/apache/activemq/artemis/api/core/management/ClusterConnectionControl.java
+++ artemis-core-client/src/main/java/org/apache/activemq/artemis/api/core/management/ClusterConnectionControl.java
@@ -141,8 +141,8 @@ public interface ClusterConnectionControl extends ActiveMQComponentControl {
     *
     * @throws Exception
     */
-   @Attribute(desc = "The metrics for the bridge by nodeId. The messagesPendingAcknowledgement counter is incremented when the bridge is has forwarded a message but is waiting acknowledgement from the other broker. The messagesAcknowledged counter is the number of messages actually received by the remote broker for this bridge.")
-   Map<String, Object> getBridgeMetrics(String nodeId) throws Exception;
+   @Operation(desc = "The metrics for the bridge by nodeId. The messagesPendingAcknowledgement counter is incremented when the bridge is has forwarded a message but is waiting acknowledgement from the other broker. The messagesAcknowledged counter is the number of messages actually received by the remote broker for this bridge.")
+   Map<String, Object> getBridgeMetrics(@Parameter(name = "nodeId", desc = "The target node ID") String nodeId) throws Exception;
 
    @Attribute(desc = "The Producer Window Size used by the Cluster Connection")
    long getProducerWindowSize();
