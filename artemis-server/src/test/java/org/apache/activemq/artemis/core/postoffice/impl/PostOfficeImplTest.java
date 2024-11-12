ee9c279789483390303b896ea45a9a9fa1a883f0
Robbie Gemmell <robbie@apache.org>
2024-11-12 14:23:46 +0000

    ARTEMIS-5142 tighten checks on results, add more tests w/both min & max set

--- artemis-server/src/test/java/org/apache/activemq/artemis/core/postoffice/impl/PostOfficeImplTest.java
+++ artemis-server/src/test/java/org/apache/activemq/artemis/core/postoffice/impl/PostOfficeImplTest.java
@@ -22,9 +22,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PostOfficeImplTest {
+
+   private static final int EXPIRATION_DELTA = 5000;
 
    @Test
    public void testZeroMaxExpiryDelayWhenExpirationNotSet() {
@@ -46,20 +49,38 @@ public class PostOfficeImplTest {
    public void testNonZeroMaxExpiryDelayWhenExpirationNotSet() {
       Message mockMessage = Mockito.mock(Message.class);
       Mockito.when(mockMessage.getExpiration()).thenReturn(0L);
-      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMaxExpiryDelay(1234L));
+      final long maxExpiryDelay = 123456L;
+      final long startTime = System.currentTimeMillis();
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMaxExpiryDelay(maxExpiryDelay));
+
       final ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
       Mockito.verify(mockMessage).setExpiration(captor.capture());
-      assertTrue(captor.getValue() > 0L);
+
+      final long expectedExpirationLow = startTime + maxExpiryDelay;
+      final long expectedExpirationHigh = expectedExpirationLow + EXPIRATION_DELTA; // Allowing a delta
+      final Long actualExpirationSet = captor.getValue();
+
+      assertExpirationSetAsExpected(expectedExpirationLow, expectedExpirationHigh, actualExpirationSet);
    }
 
    @Test
    public void testNonZeroMaxExpiryDelayWhenExpirationSet() {
       Message mockMessage = Mockito.mock(Message.class);
       Mockito.when(mockMessage.getExpiration()).thenReturn(Long.MAX_VALUE);
-      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMaxExpiryDelay(1234L));
+      final long maxExpiryDelay = 123456L;
+      final long startTime = System.currentTimeMillis();
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMaxExpiryDelay(maxExpiryDelay));
+
       final ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
       Mockito.verify(mockMessage).setExpiration(captor.capture());
-      assertTrue(captor.getValue() < Long.MAX_VALUE);
+
+      final long expectedExpirationLow = startTime + maxExpiryDelay;
+      final long expectedExpirationHigh = expectedExpirationLow + EXPIRATION_DELTA; // Allowing a delta
+      final Long actualExpirationSet = captor.getValue();
+
+      assertExpirationSetAsExpected(expectedExpirationLow, expectedExpirationHigh, actualExpirationSet);
    }
 
    @Test
@@ -82,30 +103,59 @@ public class PostOfficeImplTest {
    public void testNonZeroMinExpiryDelayWhenExpirationNotSet() {
       Message mockMessage = Mockito.mock(Message.class);
       Mockito.when(mockMessage.getExpiration()).thenReturn(0L);
-      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMinExpiryDelay(1234L));
+      final long minExpiryDelay = 123456L;
+      final long startTime = System.currentTimeMillis();
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMinExpiryDelay(minExpiryDelay));
+
       final ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
       Mockito.verify(mockMessage).setExpiration(captor.capture());
-      assertTrue(captor.getValue() > 1234L);
+
+      final long expectedExpirationLow = startTime + minExpiryDelay;
+      final long expectedExpirationHigh = expectedExpirationLow + EXPIRATION_DELTA; // Allowing a delta
+      final Long actualExpirationSet = captor.getValue();
+
+      assertExpirationSetAsExpected(expectedExpirationLow, expectedExpirationHigh, actualExpirationSet);
    }
 
    @Test
    public void testNonZeroMinExpiryDelayWhenExpirationSet() {
       Message mockMessage = Mockito.mock(Message.class);
-      Mockito.when(mockMessage.getExpiration()).thenReturn(1234L);
-      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMinExpiryDelay(9999L));
+      long origExpiration = 1234L;
+      Mockito.when(mockMessage.getExpiration()).thenReturn(origExpiration);
+      final long minExpiryDelay = 123456L;
+      assertTrue(minExpiryDelay > origExpiration);
+      final long startTime = System.currentTimeMillis();
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMinExpiryDelay(minExpiryDelay));
+
       final ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
       Mockito.verify(mockMessage).setExpiration(captor.capture());
-      assertTrue(captor.getValue() > 9999L);
+
+      final long expectedExpirationLow = startTime + minExpiryDelay;
+      final long expectedExpirationHigh = expectedExpirationLow + EXPIRATION_DELTA; // Allowing a delta
+      final Long actualExpirationSet = captor.getValue();
+
+      assertExpirationSetAsExpected(expectedExpirationLow, expectedExpirationHigh, actualExpirationSet);
    }
 
    @Test
    public void testExpiryDelayWhenExpirationNotSet() {
       Message mockMessage = Mockito.mock(Message.class);
       Mockito.when(mockMessage.getExpiration()).thenReturn(0L);
-      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setExpiryDelay(9999L));
+      final long expiryDelay = 123456L;
+      final long startTime = System.currentTimeMillis();
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setExpiryDelay(expiryDelay));
+
       final ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
       Mockito.verify(mockMessage).setExpiration(captor.capture());
-      assertTrue(captor.getValue() > System.currentTimeMillis());
+
+      final long expectedExpirationLow = startTime + expiryDelay;
+      final long expectedExpirationHigh = expectedExpirationLow + EXPIRATION_DELTA; // Allowing a delta
+      final Long actualExpirationSet = captor.getValue();
+
+      assertExpirationSetAsExpected(expectedExpirationLow, expectedExpirationHigh, actualExpirationSet);
    }
 
    @Test
@@ -114,5 +164,89 @@ public class PostOfficeImplTest {
       Mockito.when(mockMessage.getExpiration()).thenReturn(1L);
       PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setExpiryDelay(9999L));
       Mockito.verify(mockMessage, Mockito.never()).setExpiration(Mockito.anyLong());
+   }
+
+   @Test
+   public void testNonZeroMinAndMaxExpiryDelayWhenExpirationNotSet() {
+      Message mockMessage = Mockito.mock(Message.class);
+      long origExpiration = 0L;
+      Mockito.when(mockMessage.getExpiration()).thenReturn(origExpiration);
+      final long minExpiryDelay = 100_000L;
+      final long maxExpiryDelay = 300_000L;
+      final long startTime = System.currentTimeMillis();
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMinExpiryDelay(minExpiryDelay).setMaxExpiryDelay(maxExpiryDelay));
+
+      final ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+      Mockito.verify(mockMessage).setExpiration(captor.capture());
+
+      final long expectedExpirationLow = startTime + maxExpiryDelay;
+      final long expectedExpirationHigh = expectedExpirationLow + EXPIRATION_DELTA; // Allowing a delta
+      final Long actualExpirationSet = captor.getValue();
+
+      assertExpirationSetAsExpected(expectedExpirationLow, expectedExpirationHigh, actualExpirationSet);
+   }
+
+   @Test
+   public void testNonZeroMinAndMaxExpiryDelayWhenExpirationSetInbetween() {
+      Message mockMessage = Mockito.mock(Message.class);
+      final long startTime = System.currentTimeMillis();
+      long origExpiration = startTime + 200_000L;
+      Mockito.when(mockMessage.getExpiration()).thenReturn(origExpiration);
+      final long minExpiryDelay = 100_000L;
+      final long maxExpiryDelay = 300_000L;
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMinExpiryDelay(minExpiryDelay).setMaxExpiryDelay(maxExpiryDelay));
+
+      Mockito.verify(mockMessage, Mockito.never()).setExpiration(Mockito.anyLong());
+   }
+
+   @Test
+   public void testNonZeroMinAndMaxExpiryDelayWhenExpirationSetAbove() {
+      Message mockMessage = Mockito.mock(Message.class);
+      final long startTime = System.currentTimeMillis();
+      long origExpiration = startTime + 400_000L;
+      Mockito.when(mockMessage.getExpiration()).thenReturn(origExpiration);
+      final long minExpiryDelay = 100_000L;
+      final long maxExpiryDelay = 300_000L;
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMinExpiryDelay(minExpiryDelay).setMaxExpiryDelay(maxExpiryDelay));
+
+      final ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+      Mockito.verify(mockMessage).setExpiration(captor.capture());
+
+      final long expectedExpirationLow = startTime + maxExpiryDelay;
+      final long expectedExpirationHigh = expectedExpirationLow + EXPIRATION_DELTA; // Allowing a delta
+      final Long actualExpirationSet = captor.getValue();
+
+      assertExpirationSetAsExpected(expectedExpirationLow, expectedExpirationHigh, actualExpirationSet);
+   }
+
+   @Test
+   public void testNonZeroMinAndMaxExpiryDelayWhenExpirationSetBelow() {
+      Message mockMessage = Mockito.mock(Message.class);
+      final long startTime = System.currentTimeMillis();
+      long origExpiration = startTime + 50_000;
+      Mockito.when(mockMessage.getExpiration()).thenReturn(origExpiration);
+      final long minExpiryDelay = 100_000L;
+      final long maxExpiryDelay = 300_000L;
+
+      PostOfficeImpl.applyExpiryDelay(mockMessage, new AddressSettings().setMinExpiryDelay(minExpiryDelay).setMaxExpiryDelay(maxExpiryDelay));
+
+      final ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+      Mockito.verify(mockMessage).setExpiration(captor.capture());
+
+      final long expectedExpirationLow = startTime + minExpiryDelay;
+      final long expectedExpirationHigh = expectedExpirationLow + EXPIRATION_DELTA; // Allowing a delta
+      final Long actualExpirationSet = captor.getValue();
+
+      assertExpirationSetAsExpected(expectedExpirationLow, expectedExpirationHigh, actualExpirationSet);
+   }
+
+   private void assertExpirationSetAsExpected(final long expectedExpirationLow, final long expectedExpirationHigh, final Long actualExpirationSet) {
+      assertNotNull(actualExpirationSet);
+
+      assertTrue(actualExpirationSet >= expectedExpirationLow, () -> "Expected set expiration of at least " + expectedExpirationLow + ", but was: " + actualExpirationSet);
+      assertTrue(actualExpirationSet < expectedExpirationHigh, "Expected set expiration less than " + expectedExpirationHigh + ", but was: " + actualExpirationSet);
    }
 }
