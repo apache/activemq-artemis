0c47c9091137e27dd251f5da66027dd79754c4ed
Clebert Suconic <clebertsuconic@apache.org>
2024-12-09 10:31:29 -0500

    ARTEMIS-4173 Adding AMQ to logger.findText on PagingTest
    This failed at least once on my CI because the numeric pattern was used in the temporary directory, just out of luck.

--- tests/db-tests/src/test/java/org/apache/activemq/artemis/tests/db/paging/PagingTest.java
+++ tests/db-tests/src/test/java/org/apache/activemq/artemis/tests/db/paging/PagingTest.java
@@ -172,8 +172,8 @@ public class PagingTest extends ParameterDBTestBase {
       if (loggerHandler != null) {
          try {
             // These are the message errors for the negative size address size
-            assertFalse(loggerHandler.findText("222214"));
-            assertFalse(loggerHandler.findText("222215"));
+            assertFalse(loggerHandler.findText("AMQ222214"));
+            assertFalse(loggerHandler.findText("AMQ222215"));
          } finally {
             loggerHandler.close();
          }
