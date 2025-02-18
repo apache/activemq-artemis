0bf7968285624a68fe679e272e976ce73daa953d
Justin Bertram <jbertram@apache.org>
2025-02-18 13:49:31 -0600

    NO-JIRA remove dead code with invalid JavaDoc

--- tests/joram-tests/src/test/java/org/objectweb/jtests/jms/conform/connection/TopicConnectionTest.java
+++ tests/joram-tests/src/test/java/org/objectweb/jtests/jms/conform/connection/TopicConnectionTest.java
@@ -25,16 +25,14 @@ import org.objectweb.jtests.jms.framework.PubSubTestCase;
 
 /**
  * Test topic-specific connection features.
- *
+ * <p>
  * Test setting of client ID which is relevant only for Durable Subscription
  */
-
 public class TopicConnectionTest extends PubSubTestCase {
 
    /**
-    * Test that a call to <code>setClientID</code> will throw an
-    * <code>IllegalStateException</code> if a client ID has already been set
-    * see JMS javadoc
+    * Test that a call to {@code setClientID} will throw an {@code IllegalStateException} if a client ID has already
+    * been set see JMS javadoc
     * http://java.sun.com/j2ee/sdk_1.3/techdocs/api/javax/jms/Connection.html#setClientID(java.lang.String)
     */
    @Test
@@ -65,8 +63,8 @@ public class TopicConnectionTest extends PubSubTestCase {
    }
 
    /**
-    * Test that a call to <code>setClientID</code> can occur only after connection creation
-    * and before any other action on the connection.
+    * Test that a call to {@code setClientID} can occur only after connection creation and before any other action on
+    * the connection.
     * <em>This test is relevant only if the ID is set by the JMS client</em>
     * see JMS javadoc
     * http://java.sun.com/j2ee/sdk_1.3/techdocs/api/javax/jms/Connection.html#setClientID(java.lang.String)
@@ -97,48 +95,4 @@ public class TopicConnectionTest extends PubSubTestCase {
          Assert.fail("Should raise a javax.jms.IllegalStateException, not a java.lang.IllegalStateException");
       }
    }
-
-   /**
-    * Test that if another connection with the same clientID is already running when
-    * <code>setClientID</code> is called, the JMS provider should detect the duplicate
-    * ID and throw an <code>InvalidClientIDException</code>
-    * <em>This test is relevant only if the ID is set by the JMS client</em>
-    * see JMS javadoc
-    * http://java.sun.com/j2ee/sdk_1.3/techdocs/api/javax/jms/Connection.html#setClientID(java.lang.String)
-    *
-    *... This test is not valid... as getClientID is caleld before setClientID
-    */
-   /*public void testSetClientID_3()
-   {
-      try
-      {
-         // we start from a clean state for the first connection
-         subscriberConnection.close();
-         subscriberConnection = null;
-
-         subscriberConnection = subscriberTCF.createTopicConnection();
-         // if the JMS provider has set a client ID, this test is not relevant
-         if (subscriberConnection.getClientID() != null)
-         {
-            return;
-         }
-         // the JMS provider has not set a client ID, so we do
-         subscriberConnection.setClientID("testSetClientID_3");
-         assertEquals("testSetClientID_3", subscriberConnection.getClientID());
-
-         // we create a new connection and try to set the same ID than for subscriberConnection
-         TopicConnection connection_2 = subscriberTCF.createTopicConnection();
-         assertTrue(connection_2.getClientID() == null);
-         connection_2.setClientID("testSetClientID_3");
-         fail("Should throw a javax.jms.InvalidClientIDException");
-      }
-      catch (InvalidClientIDException e)
-      {
-      }
-      catch (JMSException e)
-      {
-         fail(e);
-      }
-   }*/
-
 }
